bd588af6d80e3f981746f1c5cab0c15b17050fcc
Mathieu Bastian <mathieu.bastian@gmail.com>
2009-04-20 10:48:51 +0200

    Refactoring and formatting.

--- ProjectExplorer/src/org/gephi/project/explorer/ProjectExplorer.java
+++ ProjectExplorer/src/org/gephi/project/explorer/ProjectExplorer.java
@@ -1,21 +1,30 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+Copyright 2008 WebAtlas
+Authors : Mathieu Bastian, Mathieu Jacomy, Julian Bilcke
+Website : http://www.gephi.org
 
-/*
- * ProjectExplorer.java
- *
- * Created on 28 mars 2009, 14:22:36
- */
+This file is part of Gephi.
 
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.gephi.project.explorer;
 
 import org.openide.explorer.view.BeanTreeView;
 
 /**
  *
- * @author Mathieu
+ * @author Mathieu Bastian
  */
 public class ProjectExplorer extends javax.swing.JPanel {
 
--- ProjectExplorer/src/org/gephi/project/explorer/ProjectTopComponent.java
+++ ProjectExplorer/src/org/gephi/project/explorer/ProjectTopComponent.java
@@ -1,23 +1,33 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+Copyright 2008 WebAtlas
+Authors : Mathieu Bastian, Mathieu Jacomy, Julian Bilcke
+Website : http://www.gephi.org
+
+This file is part of Gephi.
+
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.gephi.project.explorer;
 
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
 import java.io.Serializable;
-import java.util.logging.ErrorManager;
 import java.util.logging.Logger;
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Projects;
 import org.openide.explorer.ExplorerManager;
-import org.openide.explorer.ExplorerUtils;
 import org.openide.explorer.view.BeanTreeView;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
-import org.openide.util.io.NbMarshalledObject;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
