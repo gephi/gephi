962106eb4efd9c696e90aa84295d4b3707404937
Mathieu Bastian <mathieu.bastian@gmail.com>
2009-07-23 11:52:41 +0200

    Tweak toolbar with a TopTab presenter.

---
+++ DesktopBranding/src/org/gephi/branding/desktop/TopTabAction.java
@@ -0,0 +1,48 @@
+/*
+Copyright 2008 WebAtlas
+Authors : Mathieu Bastian, Mathieu Jacomy, Julian Bilcke
+Website : http://www.gephi.org
+
+This file is part of Gephi.
+
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gephi.branding.desktop;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import org.openide.util.actions.Presenter;
+
+/**
+ *
+ * @author Mathieu Bastian
+ */
+public class TopTabAction  implements Presenter.Toolbar, ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+
+    @Override
+    public Component getToolbarPresenter() {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("lalal"));
+        return panel;
+    }
+
+}
