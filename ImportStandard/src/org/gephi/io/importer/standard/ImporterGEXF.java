fe8cde2b0120459d001a2f42f26eb840326e8c07
Sébastien Heymann <sebastien@chartreuse-one>
2009-09-30 00:19:12 +0200

    Minimal GEXF 1.1 import

--- ImportStandard/src/org/gephi/io/importer/standard/ImporterGEXF.java
+++ ImportStandard/src/org/gephi/io/importer/standard/ImporterGEXF.java
@@ -21,16 +21,24 @@ along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
 package org.gephi.io.importer.standard;
 
 import java.awt.Color;
+import java.util.HashMap;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
+import org.gephi.data.properties.EdgeProperties;
+import org.gephi.data.properties.NodeProperties;
 import org.gephi.io.container.EdgeDraft;
 import org.gephi.io.container.ContainerLoader;
 import org.gephi.io.container.NodeDraft;
 import org.gephi.io.importer.FileType;
+import org.gephi.io.importer.PropertiesAssociations;
+import org.gephi.io.importer.PropertyAssociation;
 import org.gephi.io.importer.XMLImporter;
 import org.gephi.io.logging.Report;
+import org.gephi.utils.longtask.LongTask;
+import org.gephi.utils.progress.Progress;
+import org.gephi.utils.progress.ProgressTicket;
 import org.openide.filesystems.FileObject;
 import org.openide.util.NbBundle;
 import org.w3c.dom.Document;
@@ -41,22 +49,63 @@ import org.w3c.dom.NodeList;
  *
  * @author Mathieu Bastian
  */
-public class ImporterGEXF implements XMLImporter {
+public class ImporterGEXF implements XMLImporter, LongTask {
+
+    //Architecture
+    private ContainerLoader container;
+    private Report report;
+    private ProgressTicket progressTicket;
+    private boolean cancel = false;
+
+    //Settings
+    private boolean keepComplexAndEmptyAttributeTypes = true;
+    private boolean automaticProperties = true;
+
+    //Attributes
+    protected PropertiesAssociations properties = new PropertiesAssociations();
+    private HashMap<String, NodeProperties> nodePropertiesAttributes;
+    private HashMap<String, EdgeProperties> edgePropertiesAttributes;
+
+    public ImporterGEXF() {
+        //Default node associations
+        properties.addNodePropertyAssociation(new PropertyAssociation<NodeProperties>(NodeProperties.LABEL, "label"));
+        properties.addNodePropertyAssociation(new PropertyAssociation<NodeProperties>(NodeProperties.X, "x"));
+        properties.addNodePropertyAssociation(new PropertyAssociation<NodeProperties>(NodeProperties.Y, "y"));
+        properties.addNodePropertyAssociation(new PropertyAssociation<NodeProperties>(NodeProperties.Y, "z"));
+        properties.addNodePropertyAssociation(new PropertyAssociation<NodeProperties>(NodeProperties.SIZE, "size"));
+
+        //Default edge associations
+        properties.addEdgePropertyAssociation(new PropertyAssociation<EdgeProperties>(EdgeProperties.LABEL, "label"));
+        properties.addEdgePropertyAssociation(new PropertyAssociation<EdgeProperties>(EdgeProperties.WEIGHT, "weight"));
+    }
 
     public void importData(Document document, ContainerLoader container, Report report) throws Exception {
+        this.container = container;
+        this.report = report;
+        this.nodePropertiesAttributes = new HashMap<String, NodeProperties>();
+        this.edgePropertiesAttributes = new HashMap<String, EdgeProperties>();
+
+        Progress.start(progressTicket);        //Progress
+
+        //Root
+        Element root = document.getDocumentElement();
 
         //XPath
         XPathFactory factory = XPathFactory.newInstance();
         XPath xpath = factory.newXPath();
 
-        //Root
-        Element root = document.getDocumentElement();
-
         XPathExpression exp = xpath.compile("./graph/nodes/node[@id and @label]");
         NodeList nodeListE = (NodeList) exp.evaluate(root, XPathConstants.NODESET);
-
+        if (cancel) {
+            return;
+        }
         exp = xpath.compile("./graph/edges/edge[@source and @target]");
         NodeList edgeListE = (NodeList) exp.evaluate(root, XPathConstants.NODESET);
+        if (cancel) {
+            return;
+        }
+        int taskMax = nodeListE.getLength() + edgeListE.getLength();
+        Progress.switchToDeterminate(progressTicket, taskMax);
 
         //Nodes
         for (int i = 0; i < nodeListE.getLength(); i++) {
@@ -64,13 +113,13 @@ public class ImporterGEXF implements XMLImporter {
             NodeDraft node = container.factory().newNodeDraft();
 
             //Id
-            int nodeId = Integer.parseInt(nodeE.getAttribute("id"));
-            node.setId(String.valueOf(nodeId));
+            String nodeId = nodeE.getAttribute("id");
+            node.setId(nodeId);
 
             //Parent
             if (!nodeE.getAttribute("pid").isEmpty() && !nodeE.getAttribute("pid").equals("0")) {
-                int parentId = Integer.parseInt(nodeE.getAttribute("pid"));
-                node.setParent(container.getNode(String.valueOf(parentId)));
+                String parentId = nodeE.getAttribute("pid");
+                node.setParent(container.getNode(parentId));
                 container.addNode(node);
             } else {
                 container.addNode(node);
@@ -115,16 +164,16 @@ public class ImporterGEXF implements XMLImporter {
 
             //Id
             if (!edgeE.getAttribute("id").isEmpty()) {
-                int edgeId = -1;
-                edgeId = Integer.parseInt(edgeE.getAttribute("id"));
-                edge.setId(String.valueOf(edgeId));
+                String edgeId = "-1";
+                edgeId = edgeE.getAttribute("id");
+                edge.setId(edgeId);
             }
 
-            int edgeSource = Integer.parseInt(edgeE.getAttribute("source"));
-            int edgeTarget = Integer.parseInt(edgeE.getAttribute("target"));
+            String edgeSource = edgeE.getAttribute("source");
+            String edgeTarget = edgeE.getAttribute("target");
 
-            NodeDraft nodeSource = container.getNode(String.valueOf(edgeSource));
-            NodeDraft nodeTarget = container.getNode(String.valueOf(edgeTarget));
+            NodeDraft nodeSource = container.getNode(edgeSource);
+            NodeDraft nodeTarget = container.getNode(edgeTarget);
             if (nodeSource == null || nodeTarget == null) {
                 throw new NullPointerException(edgeSource + "  " + edgeTarget);
             }
@@ -132,14 +181,31 @@ public class ImporterGEXF implements XMLImporter {
             edge.setTarget(nodeTarget);
 
             //Cardinal
-            String cardinalStr = edgeE.getAttribute("cardinal");
-            if (!cardinalStr.isEmpty()) {
-                float cardinal = Float.parseFloat(cardinalStr);
+            String weightStr = edgeE.getAttribute("weight");
+            if (!weightStr.isEmpty()) {
+                float cardinal = Float.parseFloat(weightStr);
                 edge.setWeight(cardinal);
             }
 
             container.addEdge(edge);
         }
+
+        Progress.finish(progressTicket);
+
+        //Clean
+        this.container = null;
+        this.progressTicket = null;
+        this.report = null;
+        this.nodePropertiesAttributes = null;
+        this.edgePropertiesAttributes = null;
+    }
+
+    private void setNodeData(Element dataE, NodeDraft nodeDraft, String nodeId) {
+        
+    }
+
+    private void setEdgeData(Element dataE, EdgeDraft edgeDraft, String edgeId) {
+        
     }
 
     public FileType[] getFileTypes() {
@@ -149,5 +215,14 @@ public class ImporterGEXF implements XMLImporter {
 
     public boolean isMatchingImporter(FileObject fileObject) {
         return fileObject.hasExt("gexf");
+    }
+
+    public boolean cancel() {
+        cancel = true;
+        return true;
+    }
+
+    public void setProgressTicket(ProgressTicket progressTicket) {
+        this.progressTicket = progressTicket;
     }
 }
