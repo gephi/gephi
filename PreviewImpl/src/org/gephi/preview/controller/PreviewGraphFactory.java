05f1e51fd4de630f45f78a31dbaae128f6317b13
Jérémy Subtil <bigmadwolf@gmail.com>
2009-10-19 22:17:47 +0200

    PreviewGraphFactory refactoring.

--- PreviewImpl/src/org/gephi/preview/controller/PreviewGraphFactory.java
+++ PreviewImpl/src/org/gephi/preview/controller/PreviewGraphFactory.java
@@ -1,5 +1,6 @@
 package org.gephi.preview.controller;
 
+import java.util.HashMap;
 import org.gephi.preview.GraphImpl;
 import org.gephi.preview.NodeImpl;
 import org.gephi.preview.supervisor.GraphSupervisor;
@@ -10,25 +11,46 @@ import org.gephi.preview.supervisor.GraphSupervisor;
  */
 public class PreviewGraphFactory {
 
+	private final HashMap<org.gephi.graph.api.Node, NodeImpl> nodeMap = new HashMap();
+
 	public GraphImpl createPreviewGraph(org.gephi.graph.api.Graph sourceGraph, GraphSupervisor supervisor) {
 		GraphImpl previewGraph = new GraphImpl(supervisor);
 
 		for (org.gephi.graph.api.Node sourceNode : sourceGraph.getNodes()) {
-			org.gephi.graph.api.NodeData sourceNodeData = sourceNode.getNodeData();
-
-			NodeImpl previewNode = new NodeImpl(
-					previewGraph,
-					sourceNodeData.getLabel(),
-					sourceNodeData.x(),
-					sourceNodeData.y(),
-					sourceNodeData.getRadius(),
-					sourceNodeData.r(),
-					sourceNodeData.g(),
-					sourceNodeData.b());
-
+			NodeImpl previewNode = createPreviewNode(previewGraph, sourceNode);
 			previewGraph.addNode(previewNode);
 		}
 
+		for (org.gephi.graph.api.Edge sourceEdge : sourceGraph.getEdges()) {
+			if (sourceEdge.isVisible()) {
+				if (sourceEdge.isSelfLoop()) {
+					// create self-loop
+					// add self-loop
+				}
+				else {
+					// add edge
+				}
+			}
+		}
+
 		return previewGraph;
+	}
+
+	private NodeImpl createPreviewNode(GraphImpl parent, org.gephi.graph.api.Node sourceNode) {
+		org.gephi.graph.api.NodeData sourceNodeData = sourceNode.getNodeData();
+
+		NodeImpl previewNode = new NodeImpl(
+				parent,
+				sourceNodeData.getLabel(),
+				sourceNodeData.x(),
+				sourceNodeData.y(),
+				sourceNodeData.getRadius(),
+				sourceNodeData.r(),
+				sourceNodeData.g(),
+				sourceNodeData.b());
+
+		nodeMap.put(sourceNode, previewNode);
+
+		return previewNode;
 	}
 }
