fba2b8b33eded2f6338d42ec58c704b861effc67
Jérémy Subtil <bigmadwolf@gmail.com>
2009-10-21 08:37:43 +0200

    Added CubicBezierCurve implementation.

--- PreviewAPI/src/org/gephi/preview/api/CubicBezierCurve.java
+++ PreviewAPI/src/org/gephi/preview/api/CubicBezierCurve.java
@@ -3,17 +3,38 @@ package org.gephi.preview.api;
 import processing.core.PVector;
 
 /**
+ * Interface of a cubic Bézier curve.
  *
- * @author jeremy
+ * @author Jérémy Subtil <jeremy.subtil@gephi.org>
  */
 public interface CubicBezierCurve {
 
-    public PVector getPt1();
+    /**
+	 * Returns the first boundary of the curve.
+	 *
+	 * @return the first boundary of the curve
+	 */
+	public PVector getPt1();
 
+	/**
+	 * Returns the first checkpoint of the curve.
+	 *
+	 * @return the first checkpoint of the curve
+	 */
     public PVector getPt2();
-    
+
+	/**
+	 * Returns the last checkpoint of the curve.
+	 *
+	 * @return the last checkpoint of the curve
+	 */
     public PVector getPt3();
 
+	/**
+	 * Returns the last boundary of the curve.
+	 *
+	 * @return the last boundary of the curve
+	 */
     public PVector getPt4();
 
 }
