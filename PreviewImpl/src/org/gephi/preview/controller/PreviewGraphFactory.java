d2f47eae7c1b53da36b1d9c3cec1ead703d9c91d
Jérémy Subtil <bigmadwolf@gmail.com>
2009-10-28 21:42:34 +0100

    Added partial support of unidirectional edges and self-loops in the preview graph factory.

--- PreviewImpl/src/org/gephi/preview/controller/PreviewGraphFactory.java
+++ PreviewImpl/src/org/gephi/preview/controller/PreviewGraphFactory.java
@@ -3,6 +3,8 @@ package org.gephi.preview.controller;
 import java.util.HashMap;
 import org.gephi.preview.GraphImpl;
 import org.gephi.preview.NodeImpl;
+import org.gephi.preview.SelfLoopImpl;
+import org.gephi.preview.UnidirectionalEdgeImpl;
 import org.gephi.preview.supervisor.GraphSupervisor;
 
 /**
@@ -11,7 +13,7 @@ import org.gephi.preview.supervisor.GraphSupervisor;
  */
 public class PreviewGraphFactory {
 
-	private final HashMap<org.gephi.graph.api.Node, NodeImpl> nodeMap = new HashMap<org.gephi.graph.api.Node, NodeImpl>();
+	private final HashMap<Integer, NodeImpl> nodeMap = new HashMap<Integer, NodeImpl>();
 
 	public GraphImpl createPreviewGraph(org.gephi.graph.api.Graph sourceGraph, GraphSupervisor supervisor) {
 		GraphImpl previewGraph = new GraphImpl(supervisor);
@@ -21,17 +23,16 @@ public class PreviewGraphFactory {
 			previewGraph.addNode(previewNode);
 		}
 
-//		for (org.gephi.graph.api.Edge sourceEdge : sourceGraph.getEdges()) {
-//			if (sourceEdge.isVisible()) {
-//				if (sourceEdge.isSelfLoop()) {
-//					// create self-loop
-//					// add self-loop
-//				}
-//				else {
-//					// add edge
-//				}
-//			}
-//		}
+		for (org.gephi.graph.api.Edge sourceEdge : sourceGraph.getEdges()) {
+            if (sourceEdge.isSelfLoop()) {
+                SelfLoopImpl previewSelfLoop = createPreviewSelfLoop(previewGraph, sourceEdge);
+                previewGraph.addSelfLoop(previewSelfLoop);
+            }
+            else {
+                UnidirectionalEdgeImpl previewUniEdge = createPreviewUnidirectionalEdge(previewGraph, sourceEdge);
+                previewGraph.addUnidirectionalEdge(previewUniEdge);
+            }
+		}
 
 		return previewGraph;
 	}
@@ -49,8 +50,34 @@ public class PreviewGraphFactory {
 				sourceNodeData.g(),
 				sourceNodeData.b());
 
-		nodeMap.put(sourceNode, previewNode);
+		nodeMap.put(sourceNode.getId(), previewNode);
 
 		return previewNode;
+	}
+
+    private SelfLoopImpl createPreviewSelfLoop(GraphImpl parent, org.gephi.graph.api.Edge sourceSelfLoop) {
+		org.gephi.graph.api.EdgeData sourceEdgeData = sourceSelfLoop.getEdgeData();
+
+		SelfLoopImpl previewSelfLoop = new SelfLoopImpl(
+				parent,
+				sourceEdgeData.getSize(),
+                0,
+				nodeMap.get(sourceSelfLoop.getSource().getId()));
+
+		return previewSelfLoop;
+	}
+
+    private UnidirectionalEdgeImpl createPreviewUnidirectionalEdge(GraphImpl parent, org.gephi.graph.api.Edge sourceEdge) {
+		org.gephi.graph.api.EdgeData sourceEdgeData = sourceEdge.getEdgeData();
+
+		UnidirectionalEdgeImpl previewUniEdge = new UnidirectionalEdgeImpl(
+				parent,
+				sourceEdgeData.getSize(),
+                0,
+				nodeMap.get(sourceEdge.getSource().getId()),
+                nodeMap.get(sourceEdge.getTarget().getId()),
+                sourceEdgeData.getLabel());
+
+		return previewUniEdge;
 	}
 }
