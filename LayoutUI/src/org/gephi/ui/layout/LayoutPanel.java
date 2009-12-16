54bacfbb3010d1ac3ef0da66da3c802218051f06
Mathieu Bastian <mathieu.bastian@gmail.com>
2009-12-16 12:02:50 +0100

    Layout info tooltip design.

--- LayoutUI/src/org/gephi/ui/layout/LayoutPanel.java
+++ LayoutUI/src/org/gephi/ui/layout/LayoutPanel.java
@@ -20,23 +20,25 @@ along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.gephi.ui.layout;
 
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.Collections;
 import javax.swing.DefaultComboBoxModel;
-import org.gephi.layout.api.Layout;
 import org.gephi.layout.api.LayoutBuilder;
 import org.gephi.layout.api.LayoutController;
 import org.gephi.layout.api.LayoutModel;
 import org.gephi.ui.components.richtooltip.RichTooltip;
 import org.openide.explorer.propertysheet.PropertySheet;
-import org.openide.nodes.Children;
 import org.openide.nodes.Node;
+import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 
@@ -292,6 +294,66 @@ public class LayoutPanel extends javax.swing.JPanel implements PropertyChangeLis
 
     private RichTooltip buildTooltip(LayoutBuilder builder) {
         RichTooltip richTooltip = new RichTooltip(builder.getName(), builder.getDescription());
+        LayoutDescriptionImage layoutDescriptionImage = new LayoutDescriptionImage();
+        richTooltip.setMainImage(layoutDescriptionImage.getImage());
         return richTooltip;
+    }
+
+    private static class LayoutDescriptionImage {
+
+        private static final int STAR_WIDTH = 16;
+        private static final int STAR_HEIGHT = 16;
+        private static final int STAR_MAX = 5;
+        private static final int TEXT_GAP = 5;
+        private static final int LINE_GAP = 4;
+        private static final int Y_BEGIN = 10;
+        private static final int IMAGE_RIGHT_MARIN = 10;
+        private Image greenIcon;
+        private Image grayIcon;
+        private Graphics g;
+        private String qualityStr;
+        private String speedStr;
+        private int textMaxSize;
+
+        public LayoutDescriptionImage() {
+            greenIcon = ImageUtilities.loadImage("org/gephi/ui/layout/resources/yellow.png");
+            grayIcon = ImageUtilities.loadImage("org/gephi/ui/layout/resources/grey.png");
+            qualityStr = NbBundle.getMessage(LayoutPanel.class, "LayoutPanel.tooltip.quality");
+            speedStr = NbBundle.getMessage(LayoutPanel.class, "LayoutPanel.tooltip.speed");
+        }
+
+        public void paint(Graphics g) {
+
+            g.setColor(Color.BLACK);
+            g.drawString(qualityStr, 0, STAR_HEIGHT + Y_BEGIN - 2);
+            paintStarPanel(g, textMaxSize + TEXT_GAP, Y_BEGIN, 5, 3);
+            g.drawString(speedStr, 0, STAR_HEIGHT * 2 + LINE_GAP + Y_BEGIN - 2);
+            paintStarPanel(g, textMaxSize + TEXT_GAP, STAR_HEIGHT + LINE_GAP + Y_BEGIN, 5, 1);
+        }
+
+        public Image getImage() {
+            //Image size
+            BufferedImage im = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+            textMaxSize = 0;
+            textMaxSize = Math.max(im.getGraphics().getFontMetrics().stringWidth(qualityStr), textMaxSize);
+            textMaxSize = Math.max(im.getGraphics().getFontMetrics().stringWidth(speedStr), textMaxSize);
+            int imageWidth = STAR_MAX * STAR_WIDTH + TEXT_GAP + textMaxSize + IMAGE_RIGHT_MARIN;
+
+            //Paint
+            BufferedImage img = new BufferedImage(imageWidth, 100, BufferedImage.TYPE_INT_ARGB);
+            this.g = img.getGraphics();
+            paint(g);
+            return img;
+        }
+
+        public void paintStarPanel(Graphics g, int x, int y, int max, int value) {
+            for (int i = 0; i < max; i++) {
+                if (i < value) {
+                    g.drawImage(greenIcon, x + i * 16, y, null);
+                } else {
+                    g.drawImage(grayIcon, x + i * 16, y, null);
+                }
+            }
+        }
     }
 }
