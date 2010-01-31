6217446df8c3eb5e7924659f2c017e9e8fa9a2ce
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-02-01 00:02:28 +0100

    Repair ExportGEXF service declare.

--- ExportPlugin/src/org/gephi/io/exporter/plugin/ExporterGEXF.java
+++ ExportPlugin/src/org/gephi/io/exporter/plugin/ExporterGEXF.java
@@ -34,6 +34,7 @@ import org.gephi.graph.api.HierarchicalGraph;
 import org.gephi.graph.api.Node;
 import org.gephi.graph.api.NodeData;
 import org.gephi.io.exporter.api.FileType;
+import org.gephi.io.exporter.spi.GraphFileExporter;
 import org.gephi.io.exporter.spi.GraphFileExporterSettings;
 import org.gephi.io.exporter.spi.XMLGraphFileExporter;
 import org.gephi.utils.longtask.spi.LongTask;
@@ -49,7 +50,7 @@ import org.w3c.dom.Text;
  *
  * @author Sebastien Heymann
  */
-@ServiceProvider(service=XMLGraphFileExporter.class)
+@ServiceProvider(service=GraphFileExporter.class)
 public class ExporterGEXF implements XMLGraphFileExporter, LongTask {
 
     private boolean cancel = false;
