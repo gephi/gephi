ec6418b6bdbd1595b0cc5446b298ba68bf27af37
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-03-30 15:42:56 +0200

    Fix bug 551694.

--- VisualizationModule/src/org/gephi/visualization/opengl/text/TextModel.java
+++ VisualizationModule/src/org/gephi/visualization/opengl/text/TextModel.java
@@ -231,13 +231,13 @@ public class TextModel {
         String nodeFontName = nodeFontE.getAttribute("name");
         int nodeFontSize = Integer.parseInt(nodeFontE.getAttribute("size"));
         int nodeFontStyle = Integer.parseInt(nodeFontE.getAttribute("style"));
-        nodeFont = new Font(nodeFontName, nodeFontSize, nodeFontStyle);
+        nodeFont = new Font(nodeFontName, nodeFontStyle, nodeFontSize);
 
         Element edgeFontE = (Element) textModelElement.getElementsByTagName("edgefont").item(0);
         String edgeFontName = edgeFontE.getAttribute("name");
         int edgeFontSize = Integer.parseInt(edgeFontE.getAttribute("size"));
         int edgeFontStyle = Integer.parseInt(edgeFontE.getAttribute("style"));
-        edgeFont = new Font(edgeFontName, edgeFontSize, edgeFontStyle);
+        edgeFont = new Font(edgeFontName, edgeFontStyle, edgeFontSize);
 
         //Color
         Element nodeColorE = (Element) textModelElement.getElementsByTagName("nodecolor").item(0);
