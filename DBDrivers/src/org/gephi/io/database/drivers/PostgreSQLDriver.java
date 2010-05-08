90c0350e359f6cfded9de2b9151de8f62add5925
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-05-08 14:46:42 +0200

    Add PostgreSQL Driver.

---
+++ DBDrivers/src/org/gephi/io/database/drivers/PostgreSQLDriver.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gephi.io.database.drivers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Mathieu Bastian
+ */
+public class PostgreSQLDriver implements SQLDriver {
+
+    @Override
+    public Connection getConnection(String connectionUrl, String username, String passwd) throws SQLException {
+        return DriverManager.getConnection(connectionUrl, username, passwd);
+    }
+
+    @Override
+    public String getPrefix() {
+        return "postgresql";
+    }
+
+    @Override
+    public String toString() {
+        return "PostgreSQL";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SQLServerDriver) {
+            return ((SQLServerDriver) obj).getPrefix().equals(getPrefix());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return getPrefix().hashCode();
+    }
+}
