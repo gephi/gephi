60d32c99db475177889b791a1b25f66947695c09
Patrick J. McSweeney <pjmcswee@Gallifrey>
2010-05-24 13:04:02 -0400

    Strongly Connected Components added

--- StatisticsPlugin/src/org/gephi/statistics/plugin/ConnectedComponents.java
+++ StatisticsPlugin/src/org/gephi/statistics/plugin/ConnectedComponents.java
@@ -1,8 +1,23 @@
 /*
- * Author: Patrick J. McSweeney
- * Syracuse University
- */
+Copyright 2008 WebAtlas
+Authors : Patrick J. McSweeney (pjmcswee@syr.edu)
+Website : http://www.gephi.org
 
+This file is part of Gephi.
+
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.gephi.statistics.plugin;
 
 import java.util.Hashtable;
@@ -19,8 +34,6 @@ import org.gephi.graph.api.EdgeIterable;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.Node;
-import org.gephi.graph.api.NodeIterable;
-import org.gephi.graph.api.NodeIterator;
 import org.gephi.statistics.spi.Statistics;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.Progress;
@@ -31,13 +44,25 @@ import org.gephi.utils.progress.ProgressTicket;
  * @author pjmcswee
  */
 public class ConnectedComponents implements Statistics, LongTask {
+
     private boolean mDirected;
     private ProgressTicket mProgress;
     private boolean mIsCanceled;
     private String mGraphRevision;
     private int mComponentCount;
+    private int mStronglyCount;
+    int count;
 
     public void execute(GraphModel graphModel, AttributeModel attributeModel) {
+        count = 1;
+        if (mDirected) {
+            top_tarjans(graphModel, attributeModel);
+        }
+        weaklyConnected(graphModel, attributeModel);
+
+    }
+
+    public void weaklyConnected(GraphModel graphModel, AttributeModel attributeModel) {
         mIsCanceled = false;
         mComponentCount = 0;
         AttributeTable nodeTable = attributeModel.getNodeTable();
@@ -46,45 +71,38 @@ public class ConnectedComponents implements Statistics, LongTask {
             componentCol = nodeTable.addColumn("componentnumber", "Component ID", AttributeType.INT, AttributeOrigin.COMPUTED, new Integer(0));
         }
 
-        Graph graph = null;
-        if (mDirected) {
-            graph = graphModel.getDirectedGraphVisible();
-        } else {
-            graph = graphModel.getUndirectedGraphVisible();
-        }
+        Graph graph = graphModel.getUndirectedGraphVisible();
+        
 
         graph.readLock();
         mGraphRevision = "(" + graph.getNodeVersion() + ", " + graph.getEdgeVersion() + ")";
-        
+
         Hashtable<Node, Integer> indicies = new Hashtable<Node, Integer>();
         int index = 0;
         for (Node s : graph.getNodes()) {
             indicies.put(s, index);
             index++;
         }
-        
-        
+
+
         int N = graph.getNodeCount();
-   
+
         //Keep track of which nodes have been seen
         int[] color = new int[N];
-        
+
         Progress.start(mProgress, graph.getNodeCount());
-        int seenCount = 0;       
-        while(seenCount < N)
-        {
+        int seenCount = 0;
+        while (seenCount < N) {
             //The search Q
             LinkedList<Node> Q = new LinkedList<Node>();
             //The component-list
             LinkedList<Node> component = new LinkedList<Node>();
 
             //Seed the seach Q
-            for(Node first : graph.getNodes())
-            {               
-                if(color[indicies.get(first)] == 0)
-                {
+            for (Node first : graph.getNodes()) {
+                if (color[indicies.get(first)] == 0) {
                     Q.add(first);
-                    
+
                     break;
                 }
             }
@@ -100,23 +118,19 @@ public class ConnectedComponents implements Statistics, LongTask {
                 component.add(u);
 
                 //Iterate over all of u's neighbors
-                EdgeIterable edgeIter = null;
-                if (mDirected) {
-                    edgeIter = ((DirectedGraph) graph).getOutEdges(u);
-                } else {
-                    edgeIter = graph.getEdges(u);
-                }
+                EdgeIterable edgeIter =  graph.getEdges(u);
+                
                 //For each neighbor
                 for (Edge edge : edgeIter) {
                     Node reachable = graph.getOpposite(u, edge);
                     int id = indicies.get(reachable);
                     //If this neighbor is unvisited
-                    if(color[id] == 0){
+                    if (color[id] == 0) {
                         color[id] = 1;
                         //Add it to the search Q
                         Q.addLast(reachable);
                         //Mark it as used 
-                        
+
                         Progress.progress(mProgress, seenCount);
                     }
                 }
@@ -132,11 +146,92 @@ public class ConnectedComponents implements Statistics, LongTask {
         graph.readUnlock();
     }
 
-  public int getConnectedComponentsCount()
-  {
-      return mComponentCount;
-  }
-  /**
+    public void top_tarjans(GraphModel graphModel, AttributeModel attributeModel) {
+        mIsCanceled = false;
+        mStronglyCount = 0;
+        AttributeTable nodeTable = attributeModel.getNodeTable();
+        AttributeColumn componentCol = nodeTable.getColumn("stronglycomponentnumber");
+        if (componentCol == null) {
+            componentCol = nodeTable.addColumn("stronglycomponentnumber", "Strongly Component ID", AttributeType.INT, AttributeOrigin.COMPUTED, new Integer(0));
+        }
+
+        DirectedGraph graph = graphModel.getDirectedGraphVisible();
+        graph.readLock();
+        mGraphRevision = "(" + graph.getNodeVersion() + ", " + graph.getEdgeVersion() + ")";
+
+        Hashtable<Node, Integer> indicies = new Hashtable<Node, Integer>();
+        int v = 0;
+        for (Node s : graph.getNodes()) {
+            indicies.put(s, v);
+            v++;
+        }
+        int N = graph.getNodeCount();
+        int[] index = new int[N];
+        int[] low_index = new int[N];
+
+        while (true) {
+            //The search Q
+            LinkedList<Node> S = new LinkedList<Node>();
+            //The component-list
+            LinkedList<Node> component = new LinkedList<Node>();
+            //Seed the seach Q
+            Node first = null;
+            for (Node u : graph.getNodes()) {
+                if (index[indicies.get(u)] == 0) {
+                    first = u;
+                    break;
+                }
+            }
+            if (first == null) {
+                return;
+            }
+            tarjans(componentCol, S, graph, first, index, low_index, indicies);
+        }
+    }
+
+    /**
+     * 
+     * @param col
+     * @param S
+     * @param graph
+     * @param f
+     * @param index
+     * @param low_index
+     * @param indicies
+     */
+    public void tarjans(AttributeColumn col, LinkedList<Node> S,  DirectedGraph graph, Node f, int[] index, int[] low_index, Hashtable<Node, Integer> indicies) {
+        int id = indicies.get(f);
+        index[id] = count;
+        low_index[id] = count;
+        count++;
+        S.addFirst(f);
+        EdgeIterable edgeIter = graph.getOutEdges(f);
+        for (Edge e : edgeIter) {
+            Node u = graph.getOpposite(f, e);
+            int x = indicies.get(u);
+            if (index[x] == 0) {
+                tarjans(col, S, graph, u, index, low_index, indicies);
+                low_index[id] = Math.min(low_index[x], low_index[id]);
+            } else if (S.contains(u)) {
+                low_index[id] = Math.min(low_index[id], index[x]);
+            }
+        }
+        if (low_index[id] == index[id]) {
+            Node v = null;
+            while (v != f) {
+                v = S.removeFirst();
+                AttributeRow row = (AttributeRow) v.getNodeData().getAttributes();
+                row.setValue(col, mStronglyCount);
+            }
+            mStronglyCount++;
+        }
+    }
+
+    public int getConnectedComponentsCount() {
+        return mComponentCount;
+    }
+
+    /**
      *
      * @param pDirected
      */
@@ -153,7 +248,6 @@ public class ConnectedComponents implements Statistics, LongTask {
      * @return
      */
     public String getReport() {
-
         String report = new String("<HTML> <BODY> <h1>Graph Distance  Report </h1> "
                 + "<hr> <br> <h2>Network Revision Number:</h2>"
                 + mGraphRevision
@@ -161,7 +255,8 @@ public class ConnectedComponents implements Statistics, LongTask {
                 + "<h2> Parameters: </h2>"
                 + "Network Interpretation:  " + (this.mDirected ? "directed" : "undirected") + "<br>"
                 + "<br> <h2> Results: </h2>"
-                + "Connected Components: " + mComponentCount + "<br>"
+                + "Weakly Connected Components: " + mComponentCount + "<br>"
+                + "Stronlgy Connected Components: " + this.mStronglyCount + "<br>"
                 + "</BODY></HTML>");
 
         return report;
