bd36b90a39a43f05ea6384535afa230aba5ea543
Eduardo Ramos <eduramiba@gmail.com>
2010-07-03 01:11:25 +0200

    Move column values frequency html report to reusable class.

--- Data Laboratory Impl/src/org/gephi/datalaboratory/impl/manipulators/attributecolumns/ColumnValuesFrequency.java
+++ Data Laboratory Impl/src/org/gephi/datalaboratory/impl/manipulators/attributecolumns/ColumnValuesFrequency.java
@@ -21,12 +21,18 @@ along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
 package org.gephi.datalaboratory.impl.manipulators.attributecolumns;
 
 import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
 import org.gephi.data.attributes.api.AttributeColumn;
 import org.gephi.data.attributes.api.AttributeTable;
-import org.gephi.datalaboratory.impl.manipulators.attributecolumns.ui.ColumnValuesFrequencyUI;
+import org.gephi.datalaboratory.api.AttributesController;
+import org.gephi.datalaboratory.impl.utils.SimpleHTMLReport;
 import org.gephi.datalaboratory.spi.attributecolumns.AttributeColumnsManipulator;
 import org.gephi.datalaboratory.spi.attributecolumns.AttributeColumnsManipulatorUI;
 import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -39,6 +45,41 @@ import org.openide.util.lookup.ServiceProvider;
 public class ColumnValuesFrequency implements AttributeColumnsManipulator {
 
     public void execute(AttributeTable table, AttributeColumn column) {
+        Map<Object, Integer> valuesFrequencies = Lookup.getDefault().lookup(AttributesController.class).calculateColumnValuesFrequencies(table, column);
+        ArrayList<Object> values = new ArrayList<Object>(valuesFrequencies.keySet());
+
+        Collections.sort(values, new Comparator<Object>() {
+
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof Comparable) {
+                    return ((Comparable) o1).compareTo(o2);//Try to sort the values when they are comparable. (All objects of the set will have the same type).
+                } else {
+                    return 0;//Cannot sort
+                }
+            }
+        });
+
+        StringBuilder sb=new StringBuilder();
+
+        sb.append("<html><ol>");
+
+        for (Object value : values) {
+            sb.append("<li>");
+            sb.append("<b>");
+            if (value != null) {
+                sb.append(value.toString());
+            }else{
+                sb.append("null");
+            }
+            sb.append("</b> - ");
+            sb.append(valuesFrequencies.get(value));
+            sb.append("</li>");
+        }
+
+        sb.append("</ol></html>");
+        SimpleHTMLReport report=new SimpleHTMLReport(
+                NbBundle.getMessage(ColumnValuesFrequency.class, "ColumnValuesFrequency.report.title",
+                column.getTitle()),sb.toString());//Automatically show report dialog with the html.
     }
 
     public String getName() {
@@ -54,7 +95,7 @@ public class ColumnValuesFrequency implements AttributeColumnsManipulator {
     }
 
     public AttributeColumnsManipulatorUI getUI() {
-        return new ColumnValuesFrequencyUI();
+        return null;
     }
 
     public int getType() {
