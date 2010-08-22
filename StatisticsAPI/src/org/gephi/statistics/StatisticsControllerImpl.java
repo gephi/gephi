e3d7ba1ee5d508aa1defeb68bc7a1b51f80a6d92
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-08-22 23:06:56 +0200

    Update license on all files.

--- StatisticsAPI/src/org/gephi/statistics/StatisticsControllerImpl.java
+++ StatisticsAPI/src/org/gephi/statistics/StatisticsControllerImpl.java
@@ -1,23 +1,24 @@
 /*
-Copyright 2008 WebAtlas
-Authors : Mathieu Bastian, Mathieu Jacomy, Julian Bilcke, Patrick J. McSweeney (pjmcswee@syr.edu)
+Copyright 2008-2010 Gephi
+Authors : Patick J. McSweeney <pjmcswee@syr.edu>,
+          Mathieu Bastian <mathieu.bastian@gephi.org>
 Website : http://www.gephi.org
 
 This file is part of Gephi.
 
 Gephi is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
 Gephi is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 package org.gephi.statistics;
 
 import org.gephi.statistics.spi.StatisticsBuilder;
@@ -41,7 +42,8 @@ import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
- * @author Mathieu Bastian, Patrick J. McSweeney
+ * @author Mathieu Bastian
+ * @author Patrick J. McSweeney
  */
 @ServiceProvider(service = StatisticsController.class)
 public class StatisticsControllerImpl implements StatisticsController {
--- StatisticsPluginUI/src/org/gephi/ui/statistics/plugin/ConnectedComponentPanel.java
+++ StatisticsPluginUI/src/org/gephi/ui/statistics/plugin/ConnectedComponentPanel.java
@@ -1,14 +1,23 @@
 /*
- * Author: Patrick J. McSweeney
- * Syracuse University
- */
+Copyright 2008-2010 Gephi
+Authors : Patick J. McSweeney <pjmcswee@syr.edu>
+Website : http://www.gephi.org
 
-/*
- * ConnectedComponentPanel.java
- *
- * Created on May 21, 2010, 2:18:27 PM
- */
+This file is part of Gephi.
 
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.gephi.ui.statistics.plugin;
 
 /**
