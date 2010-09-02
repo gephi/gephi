311ed4d364b1bca405cb5c64a077085ee92ff28e
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-09-02 15:27:00 +0200

    Fix toString() method of TimeInterval.

--- AttributesAPI/src/org/gephi/data/attributes/type/TimeInterval.java
+++ AttributesAPI/src/org/gephi/data/attributes/type/TimeInterval.java
@@ -263,14 +263,14 @@ public final class TimeInterval extends DynamicType<Double[]> {
 	 */
 	@Override
 	public String toString() {
-		List<Double[]> list = getValues();
+		List<Interval<Double[]>> list = getIntervals(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
 		if (!list.isEmpty()) {
 			StringBuilder sb = new StringBuilder("<");
-			sb.append("[").append(list.get(0)[0]).append(", ").
-					append(list.get(0)[1]).append("]");
+			sb.append(list.get(0).isLowExcluded()?"]":"[").append(list.get(0).getLow()).append(", ").
+					append(list.get(0).getHigh()).append(list.get(0).isHighExcluded()?"[":"]");
 			for (int i = 1; i < list.size(); ++i)
-				sb.append(", ").append("[").append(list.get(i)[0]).append(", ").
-						append(list.get(i)[1]).append("]");
+				sb.append(", ").append(list.get(i).isLowExcluded()?"]":"[").append(list.get(i).getLow()).append(", ").
+						append(list.get(i).getHigh()).append(list.get(i).isHighExcluded()?"[":"]");
 			sb.append(">");
 			return sb.toString();
 		}
