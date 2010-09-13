79bcdbde6afa1a9e421700263e899c73928e814f
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-09-14 00:25:53 +0200

    Fix edge partition panel.

--- PartitionPlugin/src/org/gephi/partition/plugin/EdgeColorTransformer.java
+++ PartitionPlugin/src/org/gephi/partition/plugin/EdgeColorTransformer.java
@@ -64,6 +64,7 @@ public class EdgeColorTransformer implements Transformer {
             if (color == null) {
                 color = DEFAULT_COLOR;
             }
+            part.setColor(color);
             float r = color.getRed() / 255f;
             float g = color.getGreen() / 255f;
             float b = color.getBlue() / 255f;
