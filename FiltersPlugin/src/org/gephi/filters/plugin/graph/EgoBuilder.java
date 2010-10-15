98e7fe490c342853f703b473cc6bf19fb175b7db
Sébastien Heymann <sebastien.heymann@gmail.com>
2010-10-16 01:00:25 +0200

    EgoFilter exception fixed when a node has a null label

--- FiltersPlugin/src/org/gephi/filters/plugin/graph/EgoBuilder.java
+++ FiltersPlugin/src/org/gephi/filters/plugin/graph/EgoBuilder.java
@@ -90,7 +90,7 @@ public class EgoBuilder implements FilterBuilder {
             for (Node n : graph.getNodes()) {
                 if (n.getNodeData().getId().toLowerCase().contains(str)) {
                     nodes.add(n);
-                } else if (n.getNodeData().getLabel().toLowerCase().contains(str)) {
+                } else if ((n.getNodeData().getLabel() != null) && n.getNodeData().getLabel().toLowerCase().contains(str)) {
                     nodes.add(n);
                 }
             }
@@ -130,7 +130,7 @@ public class EgoBuilder implements FilterBuilder {
         }
 
         public String getName() {
-            return NbBundle.getMessage(DegreeRangeBuilder.class, "EgoBuilder.name");
+            return NbBundle.getMessage(EgoBuilder.class, "EgoBuilder.name");
         }
 
         public FilterProperty[] getProperties() {
