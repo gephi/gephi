bd56b31ebffb5b3eddd68a14ad243f3cb37aa61e
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-10-16 18:40:32 +0200

    Layout execution cancellation when closing workspace.

--- LayoutAPI/src/org/gephi/layout/LayoutControllerImpl.java
+++ LayoutAPI/src/org/gephi/layout/LayoutControllerImpl.java
@@ -17,7 +17,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package org.gephi.layout;
 
 import org.gephi.graph.api.GraphController;
@@ -65,6 +65,10 @@ public class LayoutControllerImpl implements LayoutController {
             }
 
             public void close(Workspace workspace) {
+                LayoutModelImpl layoutModel = workspace.getLookup().lookup(LayoutModelImpl.class);
+                if (layoutModel != null) {
+                    layoutModel.getExecutor().cancel();
+                }
             }
 
             public void disable() {
