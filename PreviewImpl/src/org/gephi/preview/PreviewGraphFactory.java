8a9e1bcbff1ff10cf122b23b3a1aa437f02a2086
Mathieu Bastian <mathieu.bastian@gmail.com>
2010-10-21 14:11:38 +0200

    Fix bug 664444.

--- PreviewImpl/src/org/gephi/preview/PreviewGraphFactory.java
+++ PreviewImpl/src/org/gephi/preview/PreviewGraphFactory.java
@@ -265,7 +265,7 @@ public class PreviewGraphFactory {
         org.gephi.graph.api.TextData sourceEdgeTextData = sourceEdgeData.getTextData();
 
         String label = sourceEdgeData.getLabel();
-        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
+        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && !sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
             label = sourceEdgeTextData.getText();
         } else if (sourceEdgeTextData != null && !sourceEdgeTextData.isVisible()) {
             label = null;
@@ -302,7 +302,7 @@ public class PreviewGraphFactory {
         org.gephi.graph.api.TextData sourceEdgeTextData = sourceEdgeData.getTextData();
 
         String label = sourceEdgeData.getLabel();
-        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
+        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && !sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
             label = sourceEdgeTextData.getText();
         } else if (sourceEdgeTextData != null && !sourceEdgeTextData.isVisible()) {
             label = null;
@@ -339,7 +339,7 @@ public class PreviewGraphFactory {
         org.gephi.graph.api.TextData sourceEdgeTextData = sourceEdgeData.getTextData();
 
         String label = sourceEdgeData.getLabel();
-        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
+        if (sourceEdgeTextData != null && sourceEdgeTextData.getText() != null && !sourceEdgeTextData.getText().isEmpty() && sourceEdgeTextData.isVisible()) {
             label = sourceEdgeTextData.getText();
         } else if (sourceEdgeTextData != null && !sourceEdgeTextData.isVisible()) {
             label = null;
