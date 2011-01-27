d5d886baf0d9ab5b389dd4276bf13cf8032c7df7
Sébastien Heymann <sebastien.heymann@gmail.com>
2011-01-27 23:37:03 +0100

    Fix bug 703877 on edge weight import for GML files

--- ImportPlugin/src/org/gephi/io/importer/plugin/file/ImporterGML.java
+++ ImportPlugin/src/org/gephi/io/importer/plugin/file/ImporterGML.java
@@ -235,7 +235,7 @@ public class ImporterGML implements FileImporter, LongTask {
             } else if ("target".equals(key)) {
                 NodeDraft target = container.getNode(value.toString());
                 edgeDraft.setTarget(target);
-            } else if ("value".equals(key)) {
+            } else if ("value".equals(key) || "weight".equals(key)) {
                 if (value instanceof Double) {
                     edgeDraft.setWeight(((Double) value).floatValue());
                 }
@@ -253,7 +253,7 @@ public class ImporterGML implements FileImporter, LongTask {
         for (int i = 0; i < list.size(); i += 2) {
             String key = (String) list.get(i);
             Object value = list.get(i + 1);
-            if ("source".equals(key) || "target".equals(key) || "value".equals(key) || "label".equals(key)) {
+            if ("source".equals(key) || "target".equals(key) || "value".equals(key) || "weight".equals(key) || "label".equals(key)) {
                 continue; // already parsed
             }
             if (value instanceof ArrayList) {
