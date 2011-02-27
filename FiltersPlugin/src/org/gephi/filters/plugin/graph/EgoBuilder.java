40df292ac802e1023d0208b497945d8841958022
Sébastien Heymann <sebastien.heymann@gmail.com>
2011-02-27 21:35:59 +0100

    Fix bug 726114 on ego network filter

--- FiltersPlugin/src/org/gephi/filters/plugin/graph/EgoBuilder.java
+++ FiltersPlugin/src/org/gephi/filters/plugin/graph/EgoBuilder.java
@@ -90,9 +90,9 @@ public class EgoBuilder implements FilterBuilder {
 
             List<Node> nodes = new ArrayList<Node>();
             for (Node n : hgraph.getNodes()) {
-                if (n.getNodeData().getId().toLowerCase().contains(str)) {
+                if (n.getNodeData().getId().toLowerCase().equals(str)) {
                     nodes.add(n);
-                } else if ((n.getNodeData().getLabel() != null) && n.getNodeData().getLabel().toLowerCase().contains(str)) {
+                } else if ((n.getNodeData().getLabel() != null) && n.getNodeData().getLabel().toLowerCase().equals(str)) {
                     nodes.add(n);
                 }
             }
