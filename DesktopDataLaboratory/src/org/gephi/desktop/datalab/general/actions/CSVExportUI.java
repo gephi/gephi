a0a764613d81153443a0ca787cdae78759927f92
Andrew Ross <andrew@dolomite>
2011-04-10 11:10:16 +0100

    Removed unreferenced jar files, and updated license headers.

--- DesktopDataLaboratory/src/org/gephi/desktop/datalab/general/actions/CSVExportUI.java
+++ DesktopDataLaboratory/src/org/gephi/desktop/datalab/general/actions/CSVExportUI.java
@@ -8,7 +8,7 @@ This file is part of Gephi.
 Gephi is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
-(at your option) any later version.
+License, or (at your option) any later version.
 
 Gephi is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,7 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 package org.gephi.desktop.datalab.general.actions;
 
 import java.nio.charset.Charset;
--- Utils/test/unit/src/org/gephi/utils/StatisticsUtilsTest.java
+++ Utils/test/unit/src/org/gephi/utils/StatisticsUtilsTest.java
@@ -10,7 +10,7 @@ This file is part of Gephi.
 Gephi is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
-(at your option) any later version.
+License, or (at your option) any later version.
 
 Gephi is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
