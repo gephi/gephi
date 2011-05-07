0f35e4801053c5f3d1cf6e5f713c5ae0c4100e16
Mathieu Bastian <mathieu.bastian@gmail.com>
2011-05-07 16:11:07 -0700

    Minor fix on GlobalSettingsPanel and increase specs version

--- VisualizationModule/src/org/gephi/visualization/component/GlobalSettingsPanel.java
+++ VisualizationModule/src/org/gephi/visualization/component/GlobalSettingsPanel.java
@@ -158,6 +158,7 @@ public class GlobalSettingsPanel extends javax.swing.JPanel {
         autoSelectNeigborCheckbox.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
         autoSelectNeigborCheckbox.setMargin(new java.awt.Insets(2, 0, 2, 2));
 
+        zoomPanel.setOpaque(false);
         zoomPanel.setLayout(new java.awt.GridBagLayout());
 
         labelZoom.setText(org.openide.util.NbBundle.getMessage(GlobalSettingsPanel.class, "GlobalSettingsPanel.labelZoom.text")); // NOI18N
