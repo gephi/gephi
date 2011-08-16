6745eef9500e4a9acbb3261aea8617665c42f60d
Mathieu Bastian <mathieu.bastian@gmail.com>
2011-08-15 21:27:06 -0700

    Add missing license header

--- DBDrivers/src/org/gephi/io/database/drivers/SQLiteDriver.java
+++ DBDrivers/src/org/gephi/io/database/drivers/SQLiteDriver.java
@@ -1,6 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+Copyright 2008-2010 Gephi
+Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
+Website : http://www.gephi.org
+
+This file is part of Gephi.
+
+Gephi is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Gephi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.gephi.io.database.drivers;
 
@@ -12,10 +28,10 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author mbastian
+ * @author Mathieu Bastian
  */
 public class SQLiteDriver implements SQLDriver {
-    
+
     public SQLiteDriver() {
         try {
             // load the sqlite-JDBC driver using the current class loader
@@ -24,24 +40,24 @@ public class SQLiteDriver implements SQLDriver {
             Logger.getLogger(SQLiteDriver.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @Override
     public Connection getConnection(String connectionUrl, String username, String passwd) throws SQLException {
         String url = connectionUrl.substring(connectionUrl.indexOf("//") + 2);
         connectionUrl = "jdbc:sqlite:" + url.substring(0, url.length() - 2);
         return DriverManager.getConnection(connectionUrl, username, passwd);
     }
-    
+
     @Override
     public String getPrefix() {
         return "sqlite";
     }
-    
+
     @Override
     public String toString() {
         return "SQLite";
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof SQLiteDriver) {
@@ -50,7 +66,7 @@ public class SQLiteDriver implements SQLDriver {
             return false;
         }
     }
-    
+
     @Override
     public int hashCode() {
         return getPrefix().hashCode();
