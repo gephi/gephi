3120a230d56729ffff068ecfa99e3a91ff613f59
Mathieu Bastian <mathieu.bastian@gmail.com>
2011-09-05 16:38:03 -0700

    Documentation

--- PreviewAPI/src/org/gephi/preview/spi/ItemBuilder.java
+++ PreviewAPI/src/org/gephi/preview/spi/ItemBuilder.java
@@ -25,17 +25,46 @@ import org.gephi.graph.api.Graph;
 import org.gephi.preview.api.Item;
 
 /**
- *
+ * Builds and returns items given a graph.
+ * <p>
+ * Items are the visual elements representing the graph and are built by item
+ * builders from the graph object. 
+ * <p>
+ * An item builder should build only a single type of items. Items are defined 
+ * by a type {@link Item#getType()}, which needs to be the same as the value
+ * returned by {@link #getType()}. In other words if this builder is building
+ * <code>Node.Item</code> items, it should return <code>Node.Item</code> as a type.
+ * <p>
+ * Item builders are singleton services and implementations need to add the
+ * following annotation to be recognized by the system:
+ * <p>
+ * <code>@ServiceProvider(service=ItemBuilder.class)</code>
+ * 
  * @author Yudi Xue, Mathieu Bastian
  */
 public interface ItemBuilder {
-    
+
     public static final String NODE_BUILDER = Item.NODE;
     public static final String NODE_LABEL_BUILDER = Item.NODE_LABEL;
     public static final String EDGE_BUILDER = Item.EDGE;
     public static final String EDGE_LABEL_BUILDER = Item.EDGE_LABEL;
-    
+
+    /**
+     * Build items from the <code>graph</code> and <code>attributeModel</code>.
+     * @param graph the graph to build items from
+     * @param attributeModel the attribute model associated to the graph
+     * @return an array of new items, from the same type returned by {@link #getType()}
+     */
     public Item[] getItems(Graph graph, AttributeModel attributeModel);
-    
+
+    /**
+     * Returns the type of this builder. 
+     * <p>
+     * The type should <b>always</b> match
+     * the type of <code>Item</code> the builder is building. For instance if the
+     * builder is building <code>Item.Node</code> type, this method should return
+     * <code>Item.Node</code>.
+     * @return the builder item type.
+     */
     public String getType();
 }
