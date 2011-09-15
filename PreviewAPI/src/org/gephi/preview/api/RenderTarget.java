fb5fdc77a023bee1fd930046c96c1004228a7b76
Mathieu Bastian <mathieu.bastian@gmail.com>
2011-09-14 20:40:22 -0700

    Preview documentation

--- PreviewAPI/src/org/gephi/preview/api/RenderTarget.java
+++ PreviewAPI/src/org/gephi/preview/api/RenderTarget.java
@@ -20,12 +20,29 @@ along with Gephi.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.gephi.preview.api;
 
+import org.gephi.preview.spi.Renderer;
+
 /**
- *
+ * RenderTarget is the graphic container the renderers draw into.
+ * <p>
+ * There are three types of targets: <b>Processing</b>, <b>PDF</b> or <b>SVG</b>.
+ * When the target is Processing, renderers obtain the 
+ * <a href="http://processing.googlecode.com/svn/trunk/processing/build/javadoc/core/index.html?processing/core/PGraphicsJava2D.html">PGraphicsJava2D</a> object.
+ * For the SVG target, renderers obtain Batik's <a href="http://xmlgraphics.apache.org/batik/using/dom-api.html">Document</a> instance. 
+ * As the PDF target rely on the iText library renderers obtain the <a href="http://api.itextpdf.com/itext/index.html?com/itextpdf/text/pdf/PdfContentByte.html">PdfContentByte</a>
+ * object.
+ * <p>
+ * Render targets are not drawing anything. They just make accessible the canvas
+ * renderers can draw into.
+ * <p>
+ * When render targets have specific properties, values should be set in the
+ * {@link PreviewProperties}.
+ * 
  * @author Yudi Xue, Mathieu Bastian
+ * @see Renderer
  */
 public interface RenderTarget {
-    
+
     public static final String PROCESSING_TARGET = "processing";
     public static final String SVG_TARGET = "svg";
     public static final String PDF_TARGET = "pdf";
