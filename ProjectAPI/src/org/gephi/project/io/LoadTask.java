8f9dfd51f354bea3582fc3a582d867d92dbc9735
Mathieu Bastian <mathieu.bastian@gmail.com>
2011-11-06 21:42:24 -0800

    Fix issue #465

--- ProjectAPI/src/org/gephi/project/io/LoadTask.java
+++ ProjectAPI/src/org/gephi/project/io/LoadTask.java
@@ -38,10 +38,15 @@ made subject to such option by the copyright holder.
 Contributor(s):
 
 Portions Copyrighted 2011 Gephi Consortium.
-*/
+ */
 package org.gephi.project.io;
 
+import com.sun.org.apache.xerces.internal.util.XMLChar;
 import java.io.File;
+import java.io.FilterReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import javax.xml.stream.Location;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLReporter;
@@ -95,7 +100,9 @@ public class LoadTask implements LongTask, Runnable {
                     System.out.println("Error:" + errorType + ", message : " + message);
                 }
             });
-            XMLStreamReader reader = inputFactory.createXMLStreamReader(fileObject.getInputStream());
+            InputStreamReader isReader = new InputStreamReader(fileObject.getInputStream());
+            Xml10FilterReader filterReader = new Xml10FilterReader(isReader);
+            XMLStreamReader reader = inputFactory.createXMLStreamReader(filterReader);
 
             if (!cancel) {
                 //Project instance
@@ -132,5 +139,70 @@ public class LoadTask implements LongTask, Runnable {
 
     public void setProgressTicket(ProgressTicket progressTicket) {
         this.progressTicket = progressTicket;
+    }
+
+    /**
+     * {@link FilterReader} to skip invalid xml version 1.0 characters. Valid
+     * Unicode chars for xml version 1.0 according to http://www.w3.org/TR/xml are
+     * #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD], [#x10000-#x10FFFF] . In
+     * other words - any Unicode character, excluding the surrogate blocks, FFFE,
+     * and FFFF.
+     * <p>
+     * More details on the <a href="http://info.tsachev.org/2009/05/skipping-invalid-xml-character-with.html">blog</a>
+     */
+    public class Xml10FilterReader extends FilterReader {
+
+        /**
+         * Creates filter reader which skips invalid xml characters.
+         * 
+         * @param in original reader
+         */
+        public Xml10FilterReader(Reader in) {
+            super(in);
+        }
+
+        /**
+         * Every overload of {@link Reader#read()} method delegates to this one so
+         * it is enough to override only this one. <br />
+         * To skip invalid characters this method shifts only valid chars to left
+         * and returns decreased value of the original read method. So after last
+         * valid character there will be some unused chars in the buffer.
+         * 
+         * @return Number of read valid characters or <code>-1</code> if end of the
+         *         underling reader was reached.
+         */
+        @Override
+        public int read(char[] cbuf, int off, int len) throws IOException {
+            int read = super.read(cbuf, off, len);
+            /*
+             * If read chars are -1 then we have reach the end of the reader.
+             */
+            if (read == -1) {
+                return -1;
+            }
+            /*
+             * pos will show the index where chars should be moved if there are gaps
+             * from invalid characters.
+             */
+            int pos = off - 1;
+
+            for (int readPos = off; readPos < off + read; readPos++) {
+                if (XMLChar.isValid(cbuf[readPos])) {
+                    pos++;
+                } else {
+                    continue;
+                }
+                /*
+                 * If there is gap(s) move current char to its position.
+                 */
+                if (pos < readPos) {
+                    cbuf[pos] = cbuf[readPos];
+                }
+            }
+            /*
+             * Number of read valid characters.
+             */
+            return pos - off + 1;
+        }
     }
 }
