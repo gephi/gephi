7b6c813efa4579461735215c27d73f4d926c9d32
Mathieu Bastian <mathieu.bastian@gmail.com>
2012-01-10 18:25:14 -0800

    Fix issue #505

--- DBDrivers/src/org/gephi/io/database/drivers/TeradataDriver.java
+++ DBDrivers/src/org/gephi/io/database/drivers/TeradataDriver.java
@@ -67,7 +67,10 @@ public class TeradataDriver implements SQLDriver {
         if (!connectionUrl.contains("database=")) {
             String dbname = connectionUrl.substring(connectionUrl.lastIndexOf('/') + 1);
             String url = connectionUrl.substring(0, connectionUrl.lastIndexOf('/'));
-            connectionUrl = url + "database=" + dbname + ",charset=UTF8";
+            String port = url.substring(url.lastIndexOf(":") + 1);
+            port = port.isEmpty() ? "" : ",dbs_port=" + port;
+            url = url.substring(0, url.lastIndexOf(":"));
+            connectionUrl = url + "/database=" + dbname + ",charset=UTF8" + port;
         }
 
         return DriverManager.getConnection(connectionUrl, username, passwd);
