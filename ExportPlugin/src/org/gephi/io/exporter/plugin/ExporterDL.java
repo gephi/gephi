094112158d5ebe47afdafcbbe89fa993ef5d9592
Taras Klaskovsky <megaterik@gmail.com>
2012-02-09 00:18:33 +0300

    Split one undirected edge to two directed, fix nodes with same labels, fix some unreadable by parser characters

--- ExportPlugin/src/org/gephi/io/exporter/plugin/ExporterDL.java
+++ ExportPlugin/src/org/gephi/io/exporter/plugin/ExporterDL.java
@@ -1,56 +1,53 @@
 /*
-Copyright 2008-2011 Gephi
-Authors : Taras Klaskovsky <megaterik@gmail.com>
-Website : http://www.gephi.org
+ Copyright 2008-2011 Gephi
+ Authors : Taras Klaskovsky <megaterik@gmail.com>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
-*/
+ Portions Copyrighted 2011 Gephi Consortium.
+ */
 package org.gephi.io.exporter.plugin;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.gephi.data.attributes.api.AttributeModel;
-import org.gephi.graph.api.DirectedGraph;
-import org.gephi.graph.api.Edge;
-import org.gephi.graph.api.EdgeIterable;
-import org.gephi.graph.api.GraphModel;
-import org.gephi.graph.api.NodeIterable;
+import org.gephi.graph.api.*;
 import org.gephi.io.exporter.spi.CharacterExporter;
 import org.gephi.io.exporter.spi.GraphExporter;
 import org.gephi.project.api.Workspace;
@@ -110,23 +107,26 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
         progressTicket.start();
         attributeModel = workspace.getLookup().lookup(AttributeModel.class);
         graphModel = workspace.getLookup().lookup(GraphModel.class);
-        DirectedGraph graph = null;
+        Graph graph = null;
         if (exportVisible) {
-            graph = graphModel.getDirectedGraphVisible();
+            graph = graphModel.getGraphVisible();
         } else {
-            graph = graphModel.getDirectedGraph();
+            graph = graphModel.getGraph();
         }
         graph.readLock();
 
         NodeIterable nodeIterable = graph.getNodes();
 
-        //use labels only if every node has label
+        //use labels only if every node has label and no two nodes have the same label
         boolean useLabels = true;
         while (nodeIterable.iterator().hasNext()) {
             if (cancel) {
                 break;
             }
             useLabels &= (nodeIterable.iterator().next().getNodeData().getLabel() != null);
+        }
+        if (useLabels && !uniqueLabels(graph)) {
+            useLabels = false;
         }
 
         if (!cancel) {
@@ -160,7 +160,7 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
         this.writer = writer;
     }
 
-    private void saveAsEdgeList1(boolean useLabels, DirectedGraph graph) throws IOException {
+    private void saveAsEdgeList1(boolean useLabels, Graph graph) throws IOException {
         writer.write("dl\n");
         writer.write("format = edgelist1\n");
         writer.write("n = " + graph.getNodeCount() + "\n");
@@ -168,44 +168,63 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
         writer.write("labels embedded:\n");
         writer.write("data:\n");
         while (edgeIterator.iterator().hasNext()) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             Edge edge = edgeIterator.iterator().next();
             if (useLabels) {
-                writer.write(edge.getSource().getNodeData().getLabel() + " "
-                        + edge.getTarget().getNodeData().getLabel() + " " + edge.getWeight() + "\n");
+                writer.write(formatLabel(edge.getSource().getNodeData().getLabel()) + " "
+                        + formatLabel(edge.getTarget().getNodeData().getLabel()) + " " + edge.getWeight() + "\n");
             } else {
-                writer.write(edge.getSource().getNodeData().getId() + " "
-                        + edge.getTarget().getNodeData().getId() + " " + edge.getWeight() + "\n");
+                writer.write(formatLabel(edge.getSource().getNodeData().getId()) + " "
+                        + formatLabel(edge.getTarget().getNodeData().getId()) + " " + edge.getWeight() + "\n");
+            }
+
+            if (!edge.isDirected()) {
+                if (useLabels) {
+                    writer.write(formatLabel(edge.getTarget().getNodeData().getLabel()) + " "
+                            + formatLabel(edge.getSource().getNodeData().getLabel()) + " " + edge.getWeight() + "\n");
+                } else {
+                    writer.write(formatLabel(edge.getTarget().getNodeData().getId()) + " "
+                            + formatLabel(edge.getSource().getNodeData().getId()) + " " + edge.getWeight() + "\n");
+                }
             }
         }
     }
 
-    private void saveAsFullMatrix(boolean useLabels, DirectedGraph graph) throws IOException {
+    private String formatLabel(String input) {
+        String res = input.replace(' ', '_');
+        return res;
+    }
+
+    private void saveAsFullMatrix(boolean useLabels, Graph graph) throws IOException {
         writer.write("dl\n");
         writer.write("format = fullmatrix\n");
         writer.write("n = " + graph.getNodeCount() + "\n");
         int maxNumber = 0;
         NodeIterable nodeIterator = graph.getNodes();
         while (nodeIterator.iterator().hasNext()) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             maxNumber = Math.max(maxNumber, nodeIterator.iterator().next().getId());
         }
 
         boolean[] isNodeHere = new boolean[maxNumber + 1];
         nodeIterator = graph.getNodes();
         while (nodeIterator.iterator().hasNext()) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             isNodeHere[nodeIterator.iterator().next().getId()] = true;
         }
 
         int[] fullNumberOfNode = new int[graph.getNodeCount()];
         int c = 0;
         for (int i = 0; i < isNodeHere.length; i++) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             if (isNodeHere[i]) {
                 fullNumberOfNode[c++] = i;
             }
@@ -213,11 +232,13 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
         int maxLengthOfEdgeWeight = 0;
         if (makeSymmetricMatrix) {
             for (int i = 0; i < graph.getNodeCount(); i++) {
-                if (cancel)
+                if (cancel) {
                     break;
+                }
                 for (int j = 0; j < graph.getNodeCount(); j++) {
-                    if (cancel)
+                    if (cancel) {
                         break;
+                    }
                     if (graph.getEdge(graph.getNode(fullNumberOfNode[i]), graph.getNode(fullNumberOfNode[j])) != null) {
                         maxLengthOfEdgeWeight = Math.max(Float.toString(graph.getEdge(graph.getNode(fullNumberOfNode[i]), graph.getNode(fullNumberOfNode[j])).getWeight()).length(), maxLengthOfEdgeWeight);
                     }
@@ -227,8 +248,9 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
 
         writer.write("labels:\n");
         for (int i = 0; i < graph.getNodeCount(); i++) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             if (useLabels) {
                 writer.write(graph.getNode(fullNumberOfNode[i]).getNodeData().getLabel());
             } else {
@@ -241,11 +263,13 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
         writer.write("\n");
         writer.write("data:\n");
         for (int i = 0; i < graph.getNodeCount(); i++) {
-            if (cancel)
+            if (cancel) {
                 break;
+            }
             for (int j = 0; j < graph.getNodeCount(); j++) {
-                if (cancel)
+                if (cancel) {
                     break;
+                }
                 if (graph.getEdge(graph.getNode(fullNumberOfNode[i]), graph.getNode(fullNumberOfNode[j])) == null) {
                     writer.write("0 ");
                     if (makeSymmetricMatrix) {
@@ -275,5 +299,20 @@ public class ExporterDL implements GraphExporter, CharacterExporter, LongTask {
     @Override
     public void setProgressTicket(ProgressTicket progressTicket) {
         this.progressTicket = progressTicket;
+    }
+    
+    private boolean uniqueLabels(Graph graph)
+    {
+        HashSet<String> labels = new HashSet<String>();
+        for (Node node : graph.getNodes())
+        {
+            String s = node.getNodeData().getId();
+            if (s == null)
+                return false;
+            if (labels.contains(formatLabel(s)))
+                return false;
+            labels.add(formatLabel(s));
+        }
+        return true;
     }
 }
