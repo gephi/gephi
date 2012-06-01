f6872713218d72d251ba6c1cff02208e629072e0
Eduardo Ramos <eduramiba@gmail.com>
2012-06-02 00:04:38 +0200

    Make data laboratory show only visible graph option enabled by default. See Issue #634

--- DesktopDataLaboratory/src/org/gephi/desktop/datalab/DataTableTopComponent.java
+++ DesktopDataLaboratory/src/org/gephi/desktop/datalab/DataTableTopComponent.java
@@ -121,7 +121,7 @@ public class DataTableTopComponent extends TopComponent implements AWTEventListe
     private static final String DATA_LABORATORY_EDGES_NODES_LABELS = "DataLaboratory_showEdgesNodesLabels";
     private static final Color invalidFilterColor = new Color(254, 150, 150);
     private final boolean dynamicFiltering;
-    private boolean visibleOnly = false;
+    private boolean visibleOnly = true;
     private boolean useSparklines = false;
     private boolean timeIntervalGraphics = false;
     private boolean showEdgesNodesLabels = false;
@@ -150,7 +150,7 @@ public class DataTableTopComponent extends TopComponent implements AWTEventListe
 
         //Get saved preferences if existing:
         dynamicFiltering = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_DYNAMIC_FILTERING, true);
-        visibleOnly = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_ONLY_VISIBLE, false);
+        visibleOnly = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_ONLY_VISIBLE, true);
         useSparklines = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_SPARKLINES, false);
         timeIntervalGraphics = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_TIME_INTERVAL_GRAPHICS, false);
         showEdgesNodesLabels = NbPreferences.forModule(DataTableTopComponent.class).getBoolean(DATA_LABORATORY_EDGES_NODES_LABELS, false);
