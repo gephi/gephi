80fb7706ef87f950b4084cc8be0483010833fcf4
Mathieu Bastian <mathieu.bastian@gmail.com>
2012-07-07 16:43:42 -0700

    Fix issue #636

--- StatisticsPlugin/src/org/gephi/statistics/plugin/WeightedDegree.java
+++ StatisticsPlugin/src/org/gephi/statistics/plugin/WeightedDegree.java
@@ -1,43 +1,43 @@
 /*
-Copyright 2008-2011 Gephi
-Authors : Sebastien Heymann <seb@gephi.org>
-Website : http://www.gephi.org
+ Copyright 2008-2011 Gephi
+ Authors : Sebastien Heymann <seb@gephi.org>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
+ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.statistics.plugin;
 
@@ -81,9 +81,9 @@ public class WeightedDegree implements Statistics, LongTask {
     private boolean isCanceled;
     private ProgressTicket progress;
     private double avgWDegree;
-    private Map<Float, Integer> degreeDist;
-    private Map<Float, Integer> inDegreeDist;
-    private Map<Float, Integer> outDegreeDist;
+    private Map<Double, Integer> degreeDist;
+    private Map<Double, Integer> inDegreeDist;
+    private Map<Double, Integer> outDegreeDist;
 
     public double getAverageDegree() {
         return avgWDegree;
@@ -97,23 +97,23 @@ public class WeightedDegree implements Statistics, LongTask {
     public void execute(HierarchicalGraph graph, AttributeModel attributeModel) {
         isDirected = graph instanceof DirectedGraph;
         isCanceled = false;
-        degreeDist = new HashMap<Float, Integer>();
-        inDegreeDist = new HashMap<Float, Integer>();
-        outDegreeDist = new HashMap<Float, Integer>();
+        degreeDist = new HashMap<Double, Integer>();
+        inDegreeDist = new HashMap<Double, Integer>();
+        outDegreeDist = new HashMap<Double, Integer>();
 
         AttributeTable nodeTable = attributeModel.getNodeTable();
         AttributeColumn degCol = nodeTable.getColumn(WDEGREE);
         AttributeColumn inCol = nodeTable.getColumn(WINDEGREE);
         AttributeColumn outCol = nodeTable.getColumn(WOUTDEGREE);
         if (degCol == null) {
-            degCol = nodeTable.addColumn(WDEGREE, "Weighted Degree", AttributeType.INT, AttributeOrigin.COMPUTED, 0);
+            degCol = nodeTable.addColumn(WDEGREE, "Weighted Degree", AttributeType.DOUBLE, AttributeOrigin.COMPUTED, 0.0);
         }
         if (isDirected) {
             if (inCol == null) {
-                inCol = nodeTable.addColumn(WINDEGREE, "Weighted In-Degree", AttributeType.INT, AttributeOrigin.COMPUTED, 0);
+                inCol = nodeTable.addColumn(WINDEGREE, "Weighted In-Degree", AttributeType.DOUBLE, AttributeOrigin.COMPUTED, 0.0);
             }
             if (outCol == null) {
-                outCol = nodeTable.addColumn(WOUTDEGREE, "Weighted Out-Degree", AttributeType.INT, AttributeOrigin.COMPUTED, 0);
+                outCol = nodeTable.addColumn(WOUTDEGREE, "Weighted Out-Degree", AttributeType.DOUBLE, AttributeOrigin.COMPUTED, 0.0);
             }
         }
 
@@ -124,17 +124,17 @@ public class WeightedDegree implements Statistics, LongTask {
 
         for (Node n : graph.getNodes()) {
             AttributeRow row = (AttributeRow) n.getNodeData().getAttributes();
-            float totalWeight = 0;
+            double totalWeight = 0;
             if (isDirected) {
                 HierarchicalDirectedGraph hdg = graph.getGraphModel().getHierarchicalDirectedGraph();
-                float totalInWeight = 0;
-                float totalOutWeight = 0;
+                double totalInWeight = 0;
+                double totalOutWeight = 0;
                 for (Iterator it = graph.getEdgesAndMetaEdges(n).iterator(); it.hasNext();) {
                     Edge e = (Edge) it.next();
-                    if (e.getSource().equals(n)) {
+                    if (e.getSource().getNodeData().equals(n.getNodeData())) {
                         totalOutWeight += e.getWeight();
                     }
-                    if (e.getTarget().equals(n)) {
+                    if (e.getTarget().getNodeData().equals(n.getNodeData())) {
                         totalInWeight += e.getWeight();
                     }
                 }
@@ -178,7 +178,7 @@ public class WeightedDegree implements Statistics, LongTask {
 
     public String getReport() {
         String report = "";
-        
+
         if (isDirected) {
             report = getDirectedReport();
         } else {
@@ -208,7 +208,7 @@ public class WeightedDegree implements Statistics, LongTask {
                     + "<hr>"
                     + "<br> <h2> Results: </h2>"
                     + "Average Weighted Degree: " + f.format(avgWDegree)
-                    + "<br /><br />"+degreeImageFile
+                    + "<br /><br />" + degreeImageFile
                     + "</BODY></HTML>";
         }
         return report;
@@ -219,10 +219,10 @@ public class WeightedDegree implements Statistics, LongTask {
         XYSeries dSeries = ChartUtils.createXYSeries(degreeDist, "Degree Distribution");
         XYSeries idSeries = ChartUtils.createXYSeries(inDegreeDist, "In-Degree Distribution");
         XYSeries odSeries = ChartUtils.createXYSeries(outDegreeDist, "Out-Degree Distribution");
-        
+
         XYSeriesCollection dataset1 = new XYSeriesCollection();
         dataset1.addSeries(dSeries);
-        
+
         XYSeriesCollection dataset2 = new XYSeriesCollection();
         dataset2.addSeries(idSeries);
 
@@ -267,16 +267,16 @@ public class WeightedDegree implements Statistics, LongTask {
         ChartUtils.decorateChart(chart3);
         ChartUtils.scaleChart(chart3, dSeries, false);
         String outdegreeImageFile = ChartUtils.renderChart(chart3, "outdegree-distribution.png");
-        
+
         NumberFormat f = new DecimalFormat("#0.000");
 
         String report = "<HTML> <BODY> <h1>Weighted Degree Report </h1> "
                 + "<hr>"
                 + "<br> <h2> Results: </h2>"
                 + "Average Weighted Degree: " + f.format(avgWDegree)
-                + "<br /><br />"+degreeImageFile
-                + "<br /><br />"+indegreeImageFile
-                + "<br /><br />"+outdegreeImageFile
+                + "<br /><br />" + degreeImageFile
+                + "<br /><br />" + indegreeImageFile
+                + "<br /><br />" + outdegreeImageFile
                 + "</BODY></HTML>";
 
         return report;
