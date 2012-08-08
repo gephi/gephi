69facc3b204aa33b9971beb2bbf9af50d503dc43
Eduardo Ramos <eduramiba@gmail.com>
2012-08-08 19:04:13 +0200

    Fix bug in AttributeTableImpl at getting column by id + type

--- AttributesImpl/src/org/gephi/data/attributes/AttributeTableImpl.java
+++ AttributesImpl/src/org/gephi/data/attributes/AttributeTableImpl.java
@@ -1,43 +1,43 @@
 /*
-Copyright 2008-2010 Gephi
-Authors : Mathieu Bastian <mathieu.bastian@gephi.org>, Martin Škurla <bujacik@gmail.com>
-Website : http://www.gephi.org
+ Copyright 2008-2010 Gephi
+ Authors : Mathieu Bastian <mathieu.bastian@gephi.org>, Martin Škurla <bujacik@gmail.com>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
+ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.data.attributes;
 
@@ -127,8 +127,10 @@ public class AttributeTableImpl implements AttributeTable {
         AttributeColumnImpl column = new AttributeColumnImpl(this, columns.size(), id, title, type, origin, defaultValue, attributeValueDelegateProvider);
         columns.add(column);
         columnsMap.put(id, column);
+        columnsMap.put(id.toLowerCase(), column);
         if (title != null && !title.equals(id)) {
             columnsMap.put(title, column);
+            columnsMap.put(title.toLowerCase(), column);
         }
         columnsSet.put(column, column);
 
@@ -184,14 +186,16 @@ public class AttributeTableImpl implements AttributeTable {
         targetImpl.index = index;
         columns.set(index, targetImpl);
         columnsMap.put(targetImpl.id, targetImpl);
+        columnsMap.put(targetImpl.id.toLowerCase(), targetImpl);
         if (targetImpl.title != null && !targetImpl.title.equals(targetImpl.id)) {
             columnsMap.put(targetImpl.title.toLowerCase(), targetImpl);
+            columnsMap.put(targetImpl.title, targetImpl);
         }
         columnsSet.put(targetImpl, targetImpl);
 
         model.fireAttributeEvent(
                 new ColumnEvent(AttributeEvent.EventType.REPLACE_COLUMN, (AttributeColumnImpl) source));
-        
+
         //Version
         version++;
         return targetImpl;
@@ -230,6 +234,9 @@ public class AttributeTableImpl implements AttributeTable {
 
     public synchronized AttributeColumnImpl getColumn(String title, AttributeType type) {
         AttributeColumnImpl c = columnsMap.get(title.toLowerCase());
+        if (c == null) {
+            columnsMap.get(title);
+        }
         if (c != null && c.getType().equals(type)) {
             return c;
         }
