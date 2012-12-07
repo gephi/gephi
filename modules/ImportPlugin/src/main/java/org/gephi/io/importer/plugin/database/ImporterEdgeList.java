8e6e2ce55e4bb14d15f1aa53488d283a4bb314fb
Eduardo Ramos <eduramiba@gmail.com>
2012-12-07 18:02:42 +0100

    Fix Issue #691

--- modules/ImportPlugin/src/main/java/org/gephi/io/importer/plugin/database/ImporterEdgeList.java
+++ modules/ImportPlugin/src/main/java/org/gephi/io/importer/plugin/database/ImporterEdgeList.java
@@ -384,14 +384,14 @@ public class ImporterEdgeList implements DatabaseImporter {
                 break;
             case SOURCE:
                 String source = rs.getString(column);
-                if (source != null) {
+                if (source != null && !source.isEmpty()) {
                     NodeDraft sourceNode = container.getNode(source);
                     edgeDraft.setSource(sourceNode);
                 }
                 break;
             case TARGET:
                 String target = rs.getString(column);
-                if (target != null) {
+                if (target != null && !target.isEmpty()) {
                     NodeDraft targetNode = container.getNode(target);
                     edgeDraft.setTarget(targetNode);
                 }
