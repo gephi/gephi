1758293a0c4a95f0f646aa1fd4bf8e59b6fd0970
Mathieu Bastian <mathieu.bastian@gmail.com>
2012-12-26 09:11:35 +0100

    Formatting

--- modules/DesktopFilters/src/main/java/org/gephi/desktop/filters/query/QueryNode.java
+++ modules/DesktopFilters/src/main/java/org/gephi/desktop/filters/query/QueryNode.java
@@ -1,43 +1,43 @@
 /*
-Copyright 2008-2010 Gephi
-Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
-Website : http://www.gephi.org
+ Copyright 2008-2010 Gephi
+ Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
+ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.desktop.filters.query;
 
@@ -83,35 +83,35 @@ public class QueryNode extends AbstractNode {
 
 
     /*@Override
-    public PasteType getDropType(final Transferable t, int action, int index) {
-    final Node dropNode = NodeTransfer.node(t, DnDConstants.ACTION_COPY_OR_MOVE);
-    if (dropNode != null && dropNode instanceof QueryNode) {
-    return new PasteType() {
+     public PasteType getDropType(final Transferable t, int action, int index) {
+     final Node dropNode = NodeTransfer.node(t, DnDConstants.ACTION_COPY_OR_MOVE);
+     if (dropNode != null && dropNode instanceof QueryNode) {
+     return new PasteType() {
 
-    @Override
-    public Transferable paste() throws IOException {
-    return null;
-    }
-    };
-    } else if (t.isDataFlavorSupported(FilterBuilderNode.DATA_FLAVOR)) {
-    return new PasteType() {
+     @Override
+     public Transferable paste() throws IOException {
+     return null;
+     }
+     };
+     } else if (t.isDataFlavorSupported(FilterBuilderNode.DATA_FLAVOR)) {
+     return new PasteType() {
 
-    @Override
-    public Transferable paste() throws IOException {
-    try {
-    FilterBuilder builder = (FilterBuilder) t.getTransferData(FilterBuilderNode.DATA_FLAVOR);
-    FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
-    } catch (UnsupportedFlavorException ex) {
-    Exceptions.printStackTrace(ex);
-    }
+     @Override
+     public Transferable paste() throws IOException {
+     try {
+     FilterBuilder builder = (FilterBuilder) t.getTransferData(FilterBuilderNode.DATA_FLAVOR);
+     FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
+     } catch (UnsupportedFlavorException ex) {
+     Exceptions.printStackTrace(ex);
+     }
 
-    return null;
-    }
-    };
-    }
-    return null;
+     return null;
+     }
+     };
+     }
+     return null;
 
-    }*/
+     }*/
     @Override
     public Action[] getActions(boolean context) {
         //System.out.println("getActions " + context);
@@ -185,21 +185,25 @@ public class QueryNode extends AbstractNode {
         public void actionPerformed(ActionEvent e) {
             FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
             Query ancestor = query;
-            while(ancestor.getParent() != null)
+            while (ancestor.getParent() != null) {
                 ancestor = ancestor.getParent();
+            }
             duplicateQuery(filterController, null, ancestor);
 
         }
 
-        private void duplicateQuery(FilterController filterController, Query parent, Query child){
+        private void duplicateQuery(FilterController filterController, Query parent, Query child) {
             Query childQuery = filterController.createQuery(child.getFilter());
-            if(parent == null)
+            if (parent == null) {
                 filterController.add(childQuery);
-            else
+            } else {
                 filterController.setSubQuery(parent, childQuery);
-            if(child.getChildrenSlotsCount() > 0)
-                for(Query grandChild : child.getChildren())
+            }
+            if (child.getChildrenSlotsCount() > 0) {
+                for (Query grandChild : child.getChildren()) {
                     duplicateQuery(filterController, childQuery, grandChild);
+                }
+            }
         }
     }
 }
