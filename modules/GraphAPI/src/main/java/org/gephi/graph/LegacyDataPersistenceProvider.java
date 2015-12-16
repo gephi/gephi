f5d49ee1ec8af12ff786e421e2fa3b3ea74c42e7
Eduardo Ramos <eduramiba@gmail.com>
2015-12-16 22:18:55 +0100

    Fix edge color problem loading old gephi project files

--- modules/GraphAPI/src/main/java/org/gephi/graph/LegacyDataPersistenceProvider.java
+++ modules/GraphAPI/src/main/java/org/gephi/graph/LegacyDataPersistenceProvider.java
@@ -155,11 +155,11 @@ public class LegacyDataPersistenceProvider implements WorkspaceXMLPersistencePro
                     float b = Float.parseFloat(reader.getAttributeValue(null, "b"));
                     float alpha = Float.parseFloat(reader.getAttributeValue(null, "a"));
                     
-                    //I have an old .gephi file with <color r="-1.0" g="0.0" b="0.0" a="1.0"> in every <edgedata>
-                    //So it seems that old versions of Gephi stored r = -1 sometimes. I guess it means no edge color set up? So use black instead.
+                    //Old gephi versions stored r = -1 to indicate that the edge had no color (use the node color)
+                    //Since Gephi 0.9, we do that with alpha = 0
                     if(r < 0 || g < 0 || b < 0){
                         r = g = b = 0;
-                        alpha = 1;
+                        alpha = 0;
                     }
                     
                     edge.setR(r);
