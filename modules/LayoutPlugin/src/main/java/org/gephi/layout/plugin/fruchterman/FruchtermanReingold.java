4915870367db5b4d425591b0eb49663e478544f5
Alex Bowen <bowen.a@gmail.com>
2016-05-13 22:13:13 +0700

    added try/catch logic to wrap graph reentrant locks, per documentation

--- modules/LayoutPlugin/src/main/java/org/gephi/layout/plugin/fruchterman/FruchtermanReingold.java
+++ modules/LayoutPlugin/src/main/java/org/gephi/layout/plugin/fruchterman/FruchtermanReingold.java
@@ -41,8 +41,6 @@
  */
 package org.gephi.layout.plugin.fruchterman;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.gephi.graph.api.Edge;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.Node;
@@ -52,6 +50,9 @@ import org.gephi.layout.spi.Layout;
 import org.gephi.layout.spi.LayoutBuilder;
 import org.gephi.layout.spi.LayoutProperty;
 import org.openide.util.NbBundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -87,92 +88,115 @@ public class FruchtermanReingold extends AbstractLayout implements Layout {
     public void goAlgo() {
         this.graph = graphModel.getGraphVisible();
         graph.readLock();
-        Node[] nodes = graph.getNodes().toArray();
-        Edge[] edges = graph.getEdges().toArray();
+        try
+        {
+            Node[] nodes = graph.getNodes().toArray();
+            Edge[] edges = graph.getEdges().toArray();
 
-        for (Node n : nodes) {
-            if (n.getLayoutData() == null || !(n.getLayoutData() instanceof ForceVectorNodeLayoutData)) {
-                n.setLayoutData(new ForceVectorNodeLayoutData());
+            for (Node n : nodes)
+            {
+                if (n.getLayoutData() == null || !(n.getLayoutData() instanceof ForceVectorNodeLayoutData))
+                {
+                    n.setLayoutData(new ForceVectorNodeLayoutData());
+                }
+                ForceVectorNodeLayoutData layoutData = n.getLayoutData();
+                layoutData.dx = 0;
+                layoutData.dy = 0;
             }
-            ForceVectorNodeLayoutData layoutData = n.getLayoutData();
-            layoutData.dx = 0;
-            layoutData.dy = 0;
-        }
 
-        float maxDisplace = (float) (Math.sqrt(AREA_MULTIPLICATOR * area) / 10f);					// Déplacement limite : on peut le calibrer...
-        float k = (float) Math.sqrt((AREA_MULTIPLICATOR * area) / (1f + nodes.length));		// La variable k, l'idée principale du layout.
+            float maxDisplace = (float) (Math.sqrt(AREA_MULTIPLICATOR * area) / 10f);                    // Déplacement limite : on peut le calibrer...
+            float k = (float) Math.sqrt((AREA_MULTIPLICATOR * area) / (1f + nodes.length));        // La variable k, l'idée principale du layout.
 
-        for (Node N1 : nodes) {
-            for (Node N2 : nodes) {	// On fait toutes les paires de noeuds
-                if (N1 != N2) {
-                    float xDist = N1.x() - N2.x();	// distance en x entre les deux noeuds
-                    float yDist = N1.y() - N2.y();
-                    float dist = (float) Math.sqrt(xDist * xDist + yDist * yDist);	// distance tout court
+            for (Node N1 : nodes)
+            {
+                for (Node N2 : nodes)
+                {    // On fait toutes les paires de noeuds
+                    if (N1 != N2)
+                    {
+                        float xDist = N1.x() - N2.x();    // distance en x entre les deux noeuds
+                        float yDist = N1.y() - N2.y();
+                        float dist = (float) Math.sqrt(xDist * xDist + yDist * yDist);    // distance tout court
 
-                    if (dist > 0) {
-                        float repulsiveF = k * k / dist;			// Force de répulsion
-                        ForceVectorNodeLayoutData layoutData = N1.getLayoutData();
-                        layoutData.dx += xDist / dist * repulsiveF;		// on l'applique...
-                        layoutData.dy += yDist / dist * repulsiveF;
+                        if (dist > 0)
+                        {
+                            float repulsiveF = k * k / dist;            // Force de répulsion
+                            ForceVectorNodeLayoutData layoutData = N1.getLayoutData();
+                            layoutData.dx += xDist / dist * repulsiveF;        // on l'applique...
+                            layoutData.dy += yDist / dist * repulsiveF;
+                        }
                     }
                 }
             }
-        }
-        for (Edge E : edges) {
-            // Idem, pour tous les noeuds on applique la force d'attraction
+            for (Edge E : edges)
+            {
+                // Idem, pour tous les noeuds on applique la force d'attraction
 
-            Node Nf = E.getSource();
-            Node Nt = E.getTarget();
+                Node Nf = E.getSource();
+                Node Nt = E.getTarget();
 
-            float xDist = Nf.x() - Nt.x();
-            float yDist = Nf.y() - Nt.y();
-            float dist = (float) Math.sqrt(xDist * xDist + yDist * yDist);
+                float xDist = Nf.x() - Nt.x();
+                float yDist = Nf.y() - Nt.y();
+                float dist = (float) Math.sqrt(xDist * xDist + yDist * yDist);
 
-            float attractiveF = dist * dist / k;
+                float attractiveF = dist * dist / k;
 
-            if (dist > 0) {
-                ForceVectorNodeLayoutData sourceLayoutData = Nf.getLayoutData();
-                ForceVectorNodeLayoutData targetLayoutData = Nt.getLayoutData();
-                sourceLayoutData.dx -= xDist / dist * attractiveF;
-                sourceLayoutData.dy -= yDist / dist * attractiveF;
-                targetLayoutData.dx += xDist / dist * attractiveF;
-                targetLayoutData.dy += yDist / dist * attractiveF;
+                if (dist > 0)
+                {
+                    ForceVectorNodeLayoutData sourceLayoutData = Nf.getLayoutData();
+                    ForceVectorNodeLayoutData targetLayoutData = Nt.getLayoutData();
+                    sourceLayoutData.dx -= xDist / dist * attractiveF;
+                    sourceLayoutData.dy -= yDist / dist * attractiveF;
+                    targetLayoutData.dx += xDist / dist * attractiveF;
+                    targetLayoutData.dy += yDist / dist * attractiveF;
+                }
             }
-        }
-        // gravity
-        for (Node n : nodes) {
-            ForceVectorNodeLayoutData layoutData = n.getLayoutData();
-            float d = (float) Math.sqrt(n.x() * n.x() + n.y() * n.y());
-            float gf = 0.01f * k * (float) gravity * d;
-            layoutData.dx -= gf * n.x() / d;
-            layoutData.dy -= gf * n.y() / d;
-        }
-        // speed
-        for (Node n : nodes) {
-            ForceVectorNodeLayoutData layoutData = n.getLayoutData();
-            layoutData.dx *= speed / SPEED_DIVISOR;
-            layoutData.dy *= speed / SPEED_DIVISOR;
-        }
-        for (Node n : nodes) {
-            // Maintenant on applique le déplacement calculé sur les noeuds.
-            // nb : le déplacement à chaque passe "instantanné" correspond à la force : c'est une sorte d'accélération.
-            ForceVectorNodeLayoutData layoutData = n.getLayoutData();
-            float xDist = layoutData.dx;
-            float yDist = layoutData.dy;
-            float dist = (float) Math.sqrt(layoutData.dx * layoutData.dx + layoutData.dy * layoutData.dy);
-            if (dist > 0 && !n.isFixed()) {
-                float limitedDist = Math.min(maxDisplace * ((float) speed / SPEED_DIVISOR), dist);
-                n.setX(n.x() + xDist / dist * limitedDist);
-                n.setY(n.y() + yDist / dist * limitedDist);
+            // gravity
+            for (Node n : nodes)
+            {
+                ForceVectorNodeLayoutData layoutData = n.getLayoutData();
+                float d = (float) Math.sqrt(n.x() * n.x() + n.y() * n.y());
+                float gf = 0.01f * k * (float) gravity * d;
+                layoutData.dx -= gf * n.x() / d;
+                layoutData.dy -= gf * n.y() / d;
             }
+            // speed
+            for (Node n : nodes)
+            {
+                ForceVectorNodeLayoutData layoutData = n.getLayoutData();
+                layoutData.dx *= speed / SPEED_DIVISOR;
+                layoutData.dy *= speed / SPEED_DIVISOR;
+            }
+            for (Node n : nodes)
+            {
+                // Maintenant on applique le déplacement calculé sur les noeuds.
+                // nb : le déplacement à chaque passe "instantanné" correspond à la force : c'est une sorte d'accélération.
+                ForceVectorNodeLayoutData layoutData = n.getLayoutData();
+                float xDist = layoutData.dx;
+                float yDist = layoutData.dy;
+                float dist = (float) Math.sqrt(layoutData.dx * layoutData.dx + layoutData.dy * layoutData.dy);
+                if (dist > 0 && !n.isFixed())
+                {
+                    float limitedDist = Math.min(maxDisplace * ((float) speed / SPEED_DIVISOR), dist);
+                    n.setX(n.x() + xDist / dist * limitedDist);
+                    n.setY(n.y() + yDist / dist * limitedDist);
+                }
+            }
+        } finally {
+            graph.readUnlock();
         }
-        graph.readUnlock();
     }
 
     @Override
     public void endAlgo() {
-        for (Node n : graph.getNodes()) {
-            n.setLayoutData(null);
+        graph.readLock();
+        try
+        {
+            for (Node n : graph.getNodes())
+            {
+                n.setLayoutData(null);
+            }
+        } finally {
+            graph.readUnlock();
         }
     }
 
