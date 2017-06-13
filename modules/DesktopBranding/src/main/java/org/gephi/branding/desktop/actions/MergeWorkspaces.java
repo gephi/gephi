9f5db624712db5ce57400793eadd2404b63746c4
alejandro-puig <alejandro.puig01@estudiant.upf.edu>
2017-06-13 13:36:57 +0200

    Merge desktop branding button class + icon

---
+++ modules/DesktopBranding/src/main/java/org/gephi/branding/desktop/actions/MergeWorkspaces.java
@@ -0,0 +1,36 @@
+package org.gephi.branding.desktop.actions;
+
+import java.awt.event.ActionEvent;
+import org.gephi.desktop.project.api.ProjectControllerUI;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.SystemAction;
+
+public class MergeWorkspaces extends SystemAction {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Lookup.getDefault().lookup(ProjectControllerUI.class).duplicateWorkspace();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Lookup.getDefault().lookup(ProjectControllerUI.class).canDuplicateWorkspace();
+    }
+
+    @Override
+    protected String iconResource() {
+        return "org/gephi/branding/desktop/actions/resources/mergeWorkspaces.png";
+    }
+
+    @Override
+    public String getName() {
+        return NbBundle.getMessage(MergeWorkspaces.class, "CTL_MergeWorkspaces");
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null;
+    }
+}
