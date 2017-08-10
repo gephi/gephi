5b736ad62398701aab426c9d08cface468bbc645
Eduardo Ramos <eduramiba@gmail.com>
2017-08-10 19:02:31 +0200

    Fix #1768 Appearance: add partition label color transformer

---
+++ modules/AppearancePlugin/src/main/java/org/gephi/appearance/plugin/PartitionLabelColorTransformer.java
@@ -0,0 +1,81 @@
+/*
+ Copyright 2008-2013 Gephi
+ Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
+ Website : http://www.gephi.org
+
+ This file is part of Gephi.
+
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+
+ Copyright 2013 Gephi Consortium. All rights reserved.
+
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
+
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
+
+ Contributor(s):
+
+ Portions Copyrighted 2013 Gephi Consortium.
+ */
+package org.gephi.appearance.plugin;
+
+import java.awt.Color;
+import org.gephi.appearance.api.Partition;
+import org.gephi.appearance.plugin.palette.PaletteManager;
+import org.gephi.appearance.spi.PartitionTransformer;
+import org.gephi.appearance.spi.Transformer;
+import org.gephi.graph.api.Element;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author mbastian
+ */
+@ServiceProvider(service = Transformer.class)
+public class PartitionLabelColorTransformer implements PartitionTransformer<Element> {
+
+    @Override
+    public void transform(Element element, Partition partition, Object value) {
+        Color color = partition.getColor(value);
+        if (color == null) {
+            color = Color.BLACK;
+        }
+        element.getTextProperties().setColor(color);
+    }
+
+    @Override
+    public boolean isNode() {
+        return true;
+    }
+
+    @Override
+    public boolean isEdge() {
+        return true;
+    }
+
+    public PaletteManager getPaletteManager() {
+        return PaletteManager.getInstance();
+    }
+}
