23034641ff1d73cdf982df409de7582e99cdc0cb
Eduardo Ramos <eduramiba@gmail.com>
2017-08-25 18:58:44 +0200

    Update splash screen

--- modules/DesktopBranding/src/main/java/org/gephi/branding/desktop/reporter/Report.java
+++ modules/DesktopBranding/src/main/java/org/gephi/branding/desktop/reporter/Report.java
@@ -229,7 +229,7 @@ public class Report {
 
     public Element writeXml(Document document) {
         Element reportE = document.createElement("report");
-        reportE.setAttribute("version", "0.9.1");
+        reportE.setAttribute("version", "0.9.2");
 
         //Date
         Element dateE = document.createElement("date");
