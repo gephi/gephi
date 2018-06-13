6c692512c2dab07d686a4a71f628e332a815d480
alexgboboc <alexgboboc@gmail.com>
2018-06-13 06:35:34 +0200

    PageRank.getCSV logic

--- modules/StatisticsPlugin/src/main/java/org/gephi/statistics/plugin/PageRank.java
+++ modules/StatisticsPlugin/src/main/java/org/gephi/statistics/plugin/PageRank.java
@@ -60,6 +60,7 @@ import org.gephi.graph.api.NodeIterable;
 import org.gephi.graph.api.Table;
 import org.gephi.graph.api.UndirectedGraph;
 import org.gephi.statistics.spi.Statistics;
+import org.gephi.utils.CSVStringBuilder;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.Progress;
 import org.gephi.utils.progress.ProgressTicket;
@@ -384,7 +385,28 @@ public class PageRank implements Statistics, LongTask {
 
     @Override
     public String getCSV() {
-        return "";
+        
+        CSVStringBuilder csv = new CSVStringBuilder();
+        
+        Map<Double, Integer> dist = new HashMap<>();
+        for (int i = 0; i < pageranks.length; i++) {
+            Double d = pageranks[i];
+            if (dist.containsKey(d)) {
+                Integer v = dist.get(d);
+                dist.put(d, v + 1);
+            } else {
+                dist.put(d, 1);
+            }
+        }
+        
+        XYSeries dSeries = ChartUtils.createXYSeries(dist, "PageRank Distribution");
+
+        double[][] dSeriesData = dSeries.toArray();
+        
+        csv.addTable(dSeriesData, "Score", "Count", "PageRank Distribution");
+        
+        return csv.getCSV();
+        
     }
     
     /**
