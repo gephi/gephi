e1bb81370432c9b64edc0078504d5b4920928238
Mathieu Bastian <mathieu.bastian@gmail.com>
2021-12-01 13:35:54 +0100

    Fix add-key script after moving workflow filers

--- .github/workflows/release/add-key.sh
+++ .github/workflows/release/add-key.sh
@@ -13,8 +13,8 @@ security unlock-keychain -p travis gephi-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/gephi-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./.github/workflows/build/certs/apple.cer -k ~/Library/Keychains/gephi-build.keychain -T /usr/bin/codesign
-security import ./.github/workflows/build/certs/dev_id.cer -k ~/Library/Keychains/gephi-build.keychain -T /usr/bin/codesign
-security import ./.github/workflows/build/certs/dev_id.p12 -k ~/Library/Keychains/gephi-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./.github/workflows/release/certs/apple.cer -k ~/Library/Keychains/gephi-build.keychain -T /usr/bin/codesign
+security import ./.github/workflows/release/certs/dev_id.cer -k ~/Library/Keychains/gephi-build.keychain -T /usr/bin/codesign
+security import ./.github/workflows/release/certs/dev_id.p12 -k ~/Library/Keychains/gephi-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis gephi-build.keychain
