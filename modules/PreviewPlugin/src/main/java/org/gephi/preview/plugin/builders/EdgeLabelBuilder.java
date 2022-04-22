6f3832837346285f7bdeedbfa9b3ba1aab0c1241
Mathieu Bastian <mathieu.bastian@gmail.com>
2022-04-22 21:32:09 +0200

    Fix #2495

--- modules/PreviewPlugin/src/main/java/org/gephi/preview/plugin/builders/EdgeLabelBuilder.java
+++ modules/PreviewPlugin/src/main/java/org/gephi/preview/plugin/builders/EdgeLabelBuilder.java
@@ -46,6 +46,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.gephi.graph.api.Column;
 import org.gephi.graph.api.Edge;
+import org.gephi.graph.api.EdgeIterable;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.GraphView;
@@ -57,6 +58,7 @@ import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
 import org.gephi.project.api.WorkspaceProvider;
 import org.gephi.visualization.api.VisualizationController;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -64,7 +66,7 @@ import org.openide.util.lookup.ServiceProvider;
  * @author Mathieu Bastian
  */
 @ServiceProvider(service = ItemBuilder.class)
-public class EdgeLabelBuilder implements ItemBuilder {
+public class EdgeLabelBuilder extends AbstractLabelBuilder implements ItemBuilder {
 
     @Override
     public Item[] getItems(Graph graph) {
@@ -74,48 +76,33 @@ public class EdgeLabelBuilder implements ItemBuilder {
         Column[] edgeColumns = vizController != null ? vizController.getEdgeTextColumns(workspace) : null;
 
         List<Item> items = new ArrayList<>();
-        for (Edge e : graph.getEdges()) {
-            EdgeLabelItem labelItem = new EdgeLabelItem(e);
-            String label = getLabel(e, edgeColumns, graph.getView());
-            labelItem.setData(EdgeLabelItem.LABEL, label);
-            TextProperties textData = e.getTextProperties();
-            if (textData != null) {
-                if (textData.getAlpha() != 0) {
-                    labelItem.setData(EdgeLabelItem.COLOR, textData.getColor());
-                }
+        EdgeIterable edgeIterable = graph.getEdges();
+        try {
+            for (Edge e : edgeIterable) {
+                EdgeLabelItem labelItem = new EdgeLabelItem(e);
+                String label = getLabel(e, edgeColumns, graph.getView());
+                labelItem.setData(EdgeLabelItem.LABEL, label);
+                TextProperties textData = e.getTextProperties();
+                if (textData != null) {
+                    if (textData.getAlpha() != 0) {
+                        labelItem.setData(EdgeLabelItem.COLOR, textData.getColor());
+                    }
 //                labelItem.setData(EdgeLabelItem.WIDTH, textData.getWidth());
 //                labelItem.setData(EdgeLabelItem.HEIGHT, textData.getHeight());
-                labelItem.setData(EdgeLabelItem.SIZE, textData.getSize());
-                labelItem.setData(EdgeLabelItem.VISIBLE, textData.isVisible());
-                if (textData.isVisible()) {
+                    labelItem.setData(EdgeLabelItem.SIZE, textData.getSize());
+                    labelItem.setData(EdgeLabelItem.VISIBLE, textData.isVisible());
+                    if (textData.isVisible() && !label.isEmpty()) {
+                        items.add(labelItem);
+                    }
+                } else if (!label.isEmpty()) {
                     items.add(labelItem);
                 }
-            } else if (label != null && !label.isEmpty()) {
-                items.add(labelItem);
             }
+        } catch (Exception e) {
+            edgeIterable.doBreak();
+            Exceptions.printStackTrace(e);
         }
         return items.toArray(new Item[0]);
-    }
-
-    private String getLabel(Edge e, Column[] cols, GraphView view) {
-        StringBuilder str = new StringBuilder();
-        if (cols != null) {
-            int i = 0;
-            for (Column c : cols) {
-                if (i++ > 0) {
-                    str.append(" - ");
-                }
-                Object val = e.getAttribute(c, view);
-                str.append(val != null ? val : "");
-            }
-        }
-        if (str.length() == 0) {
-            str = new StringBuilder(e.getLabel());
-        }
-        if (str == null) {
-            str = new StringBuilder();
-        }
-        return str.toString();
     }
 
     @Override
