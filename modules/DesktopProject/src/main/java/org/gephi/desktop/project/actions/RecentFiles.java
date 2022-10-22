9aff896d8c6cce74fe61fb3ee98e5a63fe3c0d92
Mathieu Bastian <mathieu.bastian@gmail.com>
2022-10-22 20:32:22 +0200

    Put projects in RecentFiles action

--- modules/DesktopProject/src/main/java/org/gephi/desktop/project/actions/RecentFiles.java
+++ modules/DesktopProject/src/main/java/org/gephi/desktop/project/actions/RecentFiles.java
@@ -46,12 +46,16 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.AbstractAction;
 import javax.swing.JComponent;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 import org.gephi.desktop.importer.api.ImportControllerUI;
 import org.gephi.desktop.mrufiles.api.MostRecentFiles;
+import org.gephi.project.api.Project;
+import org.gephi.project.api.ProjectController;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
@@ -66,8 +70,6 @@ import org.openide.util.NbBundle;
 @ActionRegistration(displayName = "#CTL_OpenRecentFiles", lazy = false)
 @ActionReference(path = "Menu/File", position = 350)
 public class RecentFiles extends AbstractAction implements DynamicMenuContent {
-
-    private static final String GEPHI_EXTENSION = "gephi";
 
     RecentFiles() {
         super(NbBundle.getMessage(RecentFiles.class, "CTL_OpenRecentFiles"));
@@ -98,32 +100,69 @@ public class RecentFiles extends AbstractAction implements DynamicMenuContent {
     }
 
     private JComponent[] createSubMenus() {
-        List<JComponent> items = new ArrayList<>();
-
-        MostRecentFiles mru = Lookup.getDefault().lookup(MostRecentFiles.class);
-        for (String filePath : mru.getMRUFileList()) {
-            final File file = new File(filePath);
-            if (file.exists()) {
-                final String fileName = file.getName();
-                JMenuItem menuItem = new JMenuItem(new AbstractAction(fileName) {
-
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        FileObject fileObject = FileUtil.toFileObject(file);
-                        if (fileObject.hasExt(GEPHI_EXTENSION)) {
-                            Actions.forID("File", "org.gephi.desktop.project.actions.OpenFile").actionPerformed(
-                                new ActionEvent(file, 0, null));
-                        } else {
-                            ImportControllerUI importController = Lookup.getDefault().lookup(ImportControllerUI.class);
-                            if (importController.getImportController().isFileSupported(file)) {
-                                importController.importFile(fileObject);
-                            }
-                        }
-                    }
-                });
-                items.add(menuItem);
+        // Projects
+        List<JMenuItem> projectsItems = new ArrayList<>();
+        ProjectController projectController = Lookup.getDefault().lookup(ProjectController.class);
+        for (Project project : projectController.getAllProjects()) {
+            if (project.isClosed() && project.hasFile()) {
+                JMenuItem menuItem = new JMenuItem(new OpenProjectAction(project));
+                projectsItems.add(menuItem);
             }
         }
+
+        // Files
+        MostRecentFiles mru = Lookup.getDefault().lookup(MostRecentFiles.class);
+        List<JMenuItem> filesItems = mru.getMRUFileList().stream().map(File::new).filter(File::exists)
+            .map(f -> new JMenuItem(new OpenFileAction(f))).collect(
+                Collectors.toList());
+
+        // Add to menu, with separator if needed
+        List<JComponent> items = new ArrayList<>(projectsItems);
+        if (!projectsItems.isEmpty() && !filesItems.isEmpty()) {
+            items.add(new JSeparator());
+        }
+        items.addAll(filesItems);
+
         return items.toArray(new JComponent[0]);
+    }
+
+    private static class OpenFileAction extends AbstractAction {
+
+        private final File file;
+
+        public OpenFileAction(File file) {
+            super(file.getName());
+            this.file = file;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            FileObject fileObject = FileUtil.toFileObject(file);
+            ImportControllerUI importController = Lookup.getDefault().lookup(ImportControllerUI.class);
+            if (importController.getImportController().isFileSupported(file)) {
+                importController.importFile(fileObject);
+            }
+        }
+    }
+
+    private static class OpenProjectAction extends AbstractAction {
+
+        private final Project project;
+
+        public OpenProjectAction(Project project) {
+            super(getActionName(project));
+            this.project = project;
+        }
+
+        private static String getActionName(Project project) {
+            return project.getName() + " (" + project.getFileName() + ")";
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            File file = project.getFile();
+            Actions.forID("File", "org.gephi.desktop.project.actions.OpenFile").actionPerformed(
+                new ActionEvent(file, 0, null));
+        }
     }
 }
