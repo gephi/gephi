f2b5b398ae694d050ea85afa23ba3d56927cf746
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2024-06-11 16:05:13 +0200

    sample pair of nodes distances

--- modules/StatisticsPlugin/src/main/java/org/gephi/statistics/plugin/ConnectedCloseness.java
+++ modules/StatisticsPlugin/src/main/java/org/gephi/statistics/plugin/ConnectedCloseness.java
@@ -42,25 +42,28 @@
 
 package org.gephi.statistics.plugin;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Random;
 import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphModel;
+import org.gephi.graph.api.Node;
 import org.gephi.statistics.spi.Statistics;
 import org.gephi.utils.longtask.spi.LongTask;
 import org.gephi.utils.progress.ProgressTicket;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 public class ConnectedCloseness implements Statistics, LongTask {
+
+    /**
+     * Algorithm settings
+     */
+    public static final double epsilon = 0.03;
+    public static final double grid_size = 10; // This is an optimization thing, it's not the graphical grid
 
     /**
      * Remembers if the Cancel function has been called.
      */
     private boolean isCanceled;
+
     /**
      * Keep track of the work done.
      */
@@ -71,22 +74,48 @@ public class ConnectedCloseness implements Statistics, LongTask {
      */
     @Override
     public void execute(GraphModel graphModel) {
-        Graph graph = graphModel.getGraphVisible();
-        execute(graph);
+        Graph g = graphModel.getGraphVisible();
+        execute(g);
     }
 
-    public void execute(Graph graph) {
+    public void execute(Graph g) {
         isCanceled = false;
 
-        graph.readLock();
-        
-        // TODO: everything
+        g.readLock();
+
+        final ArrayList<Double> pairs_of_nodes_sampled = sample_pairs_of_nodes_distances(g);
+
+        // TODO: everything else
 
         try {
             // graph.setAttribute(AVERAGE_DEGREE, avgDegree);
         } finally {
-            graph.readUnlockAll();
+            g.readUnlockAll();
         }
+    }
+
+    public ArrayList<Double> sample_pairs_of_nodes_distances(Graph g) {
+        ArrayList<Double> samples = new ArrayList<>();
+
+        if (g.getNodeCount()<2) {
+            return samples;
+        }
+        final int samples_count = g.getEdgeCount(); // We want as many samples as edges
+        if (samples_count<1) {
+            return samples;
+        }
+        Node[] nodes = g.getNodes().toArray();
+        Random random = new Random();
+        for (int i=0; i<samples_count; i++) {
+            Node n1 = nodes[random.nextInt(nodes.length)]; // node1 at random
+            Node n2;
+            do {
+                n2 = nodes[random.nextInt(nodes.length)]; // node2 at random but different from node1
+            } while (n1 == n2);
+            double d = Math.sqrt(Math.pow(n1.x()-n2.x(), 2)+Math.pow(n1.y()-n2.y(), 2));
+            samples.add(d);
+        }
+        return samples;
     }
 
     /**
