8b53d6af73cf9abb782004159be7650d0a2cc279
totetmatt <matthieu.totet@gmail.com>
2025-03-03 00:11:39 +0100

    Keep UniqueSizeTransformerPanel value as preference so it doesn't reset when changing function or workspace

--- modules/AppearancePluginUI/src/main/java/org/gephi/ui/appearance/plugin/UniqueSizeTransformerPanel.java
+++ modules/AppearancePluginUI/src/main/java/org/gephi/ui/appearance/plugin/UniqueSizeTransformerPanel.java
@@ -42,10 +42,12 @@
 
 package org.gephi.ui.appearance.plugin;
 
+import java.util.prefs.Preferences;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.gephi.appearance.api.SimpleFunction;
 import org.gephi.appearance.plugin.AbstractUniqueSizeTransformer;
+import org.openide.util.NbPreferences;
 
 /**
  * @author mbastian
@@ -58,20 +60,30 @@ public class UniqueSizeTransformerPanel extends javax.swing.JPanel {
     private javax.swing.JSpinner sizeSpinner;
     // End of variables declaration//GEN-END:variables
 
+    private ChangeListener sizeChangeListener = null;
+
+    static String getSizePreferenceKey(SimpleFunction function) {
+        return function.getId() + "_size";
+    }
+
     public UniqueSizeTransformerPanel() {
         initComponents();
-
-        sizeSpinner.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                transformer.setSize((Float) sizeSpinner.getValue());
-            }
-        });
     }
 
     public void setup(SimpleFunction function) {
         transformer = function.getTransformer();
+        Preferences preferences = NbPreferences.forModule(transformer.getClass());
+
+        sizeSpinner.setValue(preferences.getFloat(getSizePreferenceKey(function), transformer.getSize()));
+        if (sizeChangeListener != null) {
+            sizeSpinner.removeChangeListener(sizeChangeListener);
+        }
         sizeSpinner.setValue(transformer.getSize());
+        sizeChangeListener = (e -> {
+            transformer.setSize((Float) sizeSpinner.getValue());
+            preferences.putFloat(getSizePreferenceKey(function), transformer.getSize());
+        });
+        sizeSpinner.addChangeListener(sizeChangeListener);
     }
 
     /**
