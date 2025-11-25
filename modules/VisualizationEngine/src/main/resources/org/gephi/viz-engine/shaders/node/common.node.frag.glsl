8c68bd07440cd147a60b04957434b9aa4c664f6b
totetmatt <matthieu.totet@gmail.com>
2025-11-25 13:09:12 +0100

    Revert clamp

--- modules/VisualizationEngine/src/main/resources/org/gephi/viz-engine/shaders/node/common.node.frag.glsl
+++ modules/VisualizationEngine/src/main/resources/org/gephi/viz-engine/shaders/node/common.node.frag.glsl
@@ -3,5 +3,5 @@ void borderColor(inout vec4 color, vec2 position) {
     float t = 1.0 - borderSize;// inner edge of border
     float t2 = t * t;
 
-    color.rgb = r2 < t2 ? color.rgb : clamp(color.rgb * nodeBorderDarkenFactor, vec3(.1), vec3(.9));
+    color.rgb = r2 < t2 ? color.rgb : color.rgb * nodeBorderDarkenFactor;
 }
